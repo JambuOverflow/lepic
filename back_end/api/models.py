e7d0ba398a0d03383fde3b9330fd647b60a07c7f
renan <renancunhafonseca@gmail.com>
2020-11-27 19:32:53 -0300

    Add models of user and profile

--- back_end/api/models.py
+++ back_end/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.core.validators import validate_email
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Profile(models.Model):
+    owner = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    ROLES = (
+        (0, 'teacher'),
+        (1, 'professional'),
+        (2, 'researcher'),
+    )
+    role = models.SmallIntegerField(choices=ROLES, default=0)
+
+    def __str__(self):
+        return {
+            "role": self.role
+            }
