581f456f7426c0cecd87dde69887c2740be44019
VitorCantao <vitornovaes.cantao@gmail.com>
2020-12-06 00:29:46 -0300

    Add network info implementation

---
+++ mobile/test/core/network/network_info_test.dart
@@ -0,0 +1,31 @@
+import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mobile/core/network/network_info.dart';
+import 'package:mockito/mockito.dart';
+
+class MockDataConnectionChecker extends Mock implements DataConnectionChecker {}
+
+void main() {
+  MockDataConnectionChecker mockDataConnectionChecker;
+  NetworkInfoImpl networkInfo;
+
+  setUp(() {
+    mockDataConnectionChecker = MockDataConnectionChecker();
+    networkInfo = NetworkInfoImpl(mockDataConnectionChecker);
+  });
+
+  group('isConnected', () {
+    test('should forward the call to DataConnectionChecker.hasConnection',
+        () async {
+      final tHasConnectionFuture = Future.value(true);
+
+      when(mockDataConnectionChecker.hasConnection)
+          .thenAnswer((_) async => tHasConnectionFuture);
+
+      final result = await networkInfo.isConnected;
+
+      verify(mockDataConnectionChecker.hasConnection);
+      expect(result, tHasConnectionFuture);
+    });
+  });
+}
