243e0c8989a1af5a88b207c0f99a176749cfc323
VitorCantao <vitornovaes.cantao@gmail.com>
2020-12-06 13:37:05 -0300

    Create update classroom use case test

---
+++ mobile/test/features/class_management/domain/use_cases/update_classroom_use_case_test.dart
@@ -0,0 +1,48 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mobile/core/network/response.dart';
+import 'package:mobile/features/class_management/domain/entities/classroom.dart';
+import 'package:mobile/features/class_management/domain/repositories/classroom_repository.dart';
+import 'package:mobile/features/class_management/domain/use_cases/classroom_params.dart';
+import 'package:mobile/features/class_management/domain/use_cases/update_classroom_use_case.dart';
+import 'package:mobile/features/user_management/domain/entities/user.dart';
+import 'package:mockito/mockito.dart';
+
+class MockClassroomRepository extends Mock implements ClassroomRepository {}
+
+void main() {
+  UpdateClassroom useCase;
+  MockClassroomRepository mockClassroomRepository;
+
+  setUp(() {
+    mockClassroomRepository = MockClassroomRepository();
+    useCase = UpdateClassroom(repository: mockClassroomRepository);
+  });
+
+  final tUser = User(
+    firstName: 'v',
+    lastName: 'c',
+    email: 'v@g.com',
+    role: Role.teacher,
+    password: '123',
+  );
+
+  final tClassroom = Classroom(
+    tutor: tUser,
+    grade: 1,
+    name: "A",
+  );
+
+  final tResponse = Response(message: "Nice");
+
+  test('should return a correct response when updating a classroom', () async {
+    when(mockClassroomRepository.updateClassroom(tClassroom))
+        .thenAnswer((_) async => Right(tResponse));
+
+    final result = await useCase(ClassroomParams(classroom: tClassroom));
+
+    expect(result, Right(tResponse));
+    verify(mockClassroomRepository.updateClassroom(tClassroom));
+    verifyNoMoreInteractions(mockClassroomRepository);
+  });
+}
