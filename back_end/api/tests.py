95668a4048565d04c83827f71875b31b7c47d23e
Arouck <ppvitorr@gmail.com>
2020-12-08 19:04:30 -0300

    Create CRUD user and token tests List, create and update (put and patch) users and get token

--- back_end/api/tests.py
+++ back_end/api/tests.py
@@ -1,11 +1,18 @@
-from .views import create_class, get_classes
-from rest_framework.test import APIRequestFactory
-import pytest
+#from .views import create_class, get_classes
+#from rest_framework.test import APIRequestFactory
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
+from .models import User
+#from rest_framework.test import APIRequestFactory
+#import pytest
 
 # Create your tests here.
 
 
-class TestCRUDClass:
+'''class TestCRUDClass:
     def test_create_class(self):
         new_class = {
             "grade": 3,
@@ -13,7 +20,127 @@ class TestCRUDClass:
         }
         factory = APIRequestFactory()
         response = factory.post('/api/classes', new_class, format='json')
-        assert response.status_code == 200
+        assert response.status_code == 200'''
 
 
+class TestCrudUser(APITestCase):
 
+    def test_list_users(self):
+        self.client.credentials()
+        url = reverse('list-and-create-users')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual([], response.data)
+
+    def test_create_user(self):
+        self.client.credentials()
+        url = reverse('list-and-create-users')
+        data = {
+            "first_name": "Arthur",
+            "last_name": "Takeshi",
+            "email": "takeshi@ufpa.br",
+            "username" : "arthur",
+            "password" : "arthur",
+            "user_role": 3
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().username, 'arthur')
+        self.assertEqual(User.objects.get().password == 'arthur', False)
+
+    def test_get_token(self):
+        self.client.credentials()
+        url = reverse('list-and-create-users')
+        data = {
+            "first_name": "Arthur",
+            "last_name": "Takeshi",
+            "email": "takeshi@ufpa.br",
+            "username" : "arthur",
+            "password" : "arthur",
+            "user_role": 3
+        }
+        response = self.client.post(url, data, format='json')
+        url = reverse('get-user-token')
+        data = {
+            "username": "arthur",
+            "password": "arthur"
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual('token' in response.data, True)
+
+    def test_update_user_put(self):
+        self.client.credentials()
+        url = reverse('list-and-create-users')
+        data = {
+            "first_name": "Arthur",
+            "last_name": "Takeshi",
+            "email": "takeshi@ufpa.br",
+            "username" : "arthur",
+            "password" : "arthur",
+            "user_role": 3
+        }
+        response = self.client.post(url, data, format='json')
+        old_username = response.data['username']
+        old_password = response.data['password']
+
+        url = reverse('get-user-token')
+        data = {
+            "username": "arthur",
+            "password": "arthur"
+        }
+        response = self.client.post(url, data, format='json')
+        token = response.data['token']
+
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+        url = reverse('update-delete-users', args=[1])
+        data = {
+            "first_name": "Renan",
+            "last_name": "Cunha",
+            "email": "renan@ufpa.br",
+            "username" : "renan",
+            "password" : "renan",
+            "user_role": 1
+        }
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().username == old_username, False)
+        self.assertEqual(User.objects.get().password == old_password, False)
+
+    def test_update_user_patch(self):
+        self.client.credentials()
+        url = reverse('list-and-create-users')
+        data = {
+            "first_name": "Arthur",
+            "last_name": "Takeshi",
+            "email": "takeshi@ufpa.br",
+            "username" : "arthur",
+            "password" : "arthur",
+            "user_role": 3
+        }
+        response = self.client.post(url, data, format='json')
+        old_username = response.data['username']
+        old_password = response.data['password']
+
+        url = reverse('get-user-token')
+        data = {
+            "username": "arthur",
+            "password": "arthur"
+        }
+        response = self.client.post(url, data, format='json')
+        token = response.data['token']
+
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+        url = reverse('update-delete-users', args=[1])
+        data = {
+            "username" : "renan",
+            "password" : "renan",
+            "user_role": 1
+        }
+        response = self.client.patch(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().username == old_username, False)
+        self.assertEqual(User.objects.get().password == old_password, False)
