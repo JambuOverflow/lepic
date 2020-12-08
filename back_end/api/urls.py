175ed8197b8d299af135a06157f8ea866188325f
Arouck <ppvitorr@gmail.com>
2020-12-08 17:34:57 -0300

    Remove paths to function based views And add path to token auth

--- back_end/api/urls.py
+++ back_end/api/urls.py
@@ -1,12 +1,9 @@
 from django.urls import include, path
 from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-  path('create_user', views.create_user),
-  path('get_users', views.get_users),
-  path('update_user/<int:profile_id>', views.update_user),
-  path('test/<int:primary_key>', views.LepicUser.as_view()),
   path('classes/', views.ClassCreate.as_view()),
   path('classes/<int:pk>/', views.ClassDetail.as_view()),
   path('texts/', views.TextCreate.as_view()),
@@ -15,10 +12,11 @@ urlpatterns = [
   path('tutor/<int:pk_tutor>/classes', views.ListTutorClasses.as_view()),
   path('users/', views.UserList.as_view()),
   path('users/<int:pk>/', views.UserDetail.as_view()),
+  path('token-auth/', obtain_auth_token),
 ] 
 
 urlpatterns = format_suffix_patterns(urlpatterns)
 
-urlpatterns += [
+'''urlpatterns += [
     path('auth/', include('rest_framework.urls')),
-]
+]'''
