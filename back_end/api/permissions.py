b9248d392949e876ee914f6562497c7823ad7e2c
Arouck <ppvitorr@gmail.com>
2020-12-08 17:38:18 -0300

    Create IsOwner permission This permission allows only the account owner to read and edit it

--- back_end/api/permissions.py
+++ back_end/api/permissions.py
@@ -12,5 +12,14 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        # Write permissions are only allowed to the owner of the snippet.
+        # Write permissions are only allowed to the owner of the object.
         return obj.tutor == request.user
+
+class IsOwner(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # All permissions are only allowed to the owner of the object.
+        return obj == request.user
