577aa4eb9f5d5a381f52ec22653226b349789c49
renan <renancunhafonseca@gmail.com>
2020-12-09 14:18:18 -0300

    Add classroom entity to model map

--- mobile/test/features/class_management/data/models/classroom_model_test.dart
+++ mobile/test/features/class_management/data/models/classroom_model_test.dart
@@ -2,15 +2,15 @@ import 'dart:convert';
 
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mobile/core/data/database.dart';
-import 'package:mobile/features/class_management/data/models/classroom_model.dart';
 import 'package:mobile/features/class_management/domain/entities/classroom.dart';
-import 'package:moor/moor.dart';
-
 import '../../../../core/fixtures/fixture_reader.dart';
+import 'package:mobile/features/class_management/data/models/classroom_model.dart';
 
 void main() {
   final tClassModel =
       ClassroomModel(grade: 1, localId: 2, name: "A", tutorId: 3);
+
+  final tClassEntity = Classroom(grade: 1, id: 2, name: "A", tutorId: 3);
 
   group("from json", () {
     test("should return a valid Classroom model", () async {
@@ -34,6 +34,22 @@ void main() {
       };
 
       expect(result, expectedMap);
+    });
+  });
+
+  group('modelToEntity', () {
+    test('should return a Classroom entity with proper data', () async {
+      final result = classroomModelToEntity(tClassModel);
+
+      expect(result, tClassEntity);
+    });
+  });
+
+  group('entityToModel', () {
+    test('should return a Classroom model with proper data', () async {
+      final result = classroomEntityToModel(tClassEntity);
+
+      expect(result, tClassModel);
     });
   });
 }
