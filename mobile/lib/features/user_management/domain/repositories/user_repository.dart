1881704121c2613af61713724c5bc3529ce99f89
VitorCantao <vitornovaes.cantao@gmail.com>
2020-12-09 16:34:36 -0300

    Add login method to user repository interface

--- mobile/lib/features/user_management/domain/repositories/user_repository.dart
+++ mobile/lib/features/user_management/domain/repositories/user_repository.dart
@@ -8,4 +8,5 @@ abstract class UserRepository {
   Future<Either<Failure, User>> getStoredUser();
   Future<Either<Failure, Response>> createUser(User user);
   Future<Either<Failure, Response>> updateUser(User user, String token);
+  Future<Either<Failure, Response>> login(User user);
 }
