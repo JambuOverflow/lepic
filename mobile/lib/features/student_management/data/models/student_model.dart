4ffffec4ff38591e7d1c9107481a3c1aab8ce26a
João Canavarro <jvcanavarro@gmail.com>
2020-12-12 17:22:19 -0300

    Create student data layer

---
+++ mobile/lib/features/student_management/data/models/student_model.dart
@@ -0,0 +1,33 @@
+import 'package:mobile/core/data/database.dart';
+import 'package:mobile/features/student_management/domain/entities/student.dart';
+import 'package:moor/moor.dart';
+
+class StudentModels extends Table {
+  @JsonKey('local_id')
+  IntColumn get localId => integer().autoIncrement()();
+  @JsonKey('first_name')
+  TextColumn get firstName => text()();
+  @JsonKey('last_name')
+  TextColumn get lastName => text()();
+  @JsonKey('classroom_id')
+  IntColumn get classroomId => integer()
+      .customConstraint('NOT NULL REFERENCES classroom_models(local_id)')();
+}
+
+Student studentModelToEntity(StudentModel model) {
+  return Student(
+    firstName: model.firstName,
+    lastName: model.lastName,
+    id: model.localId,
+    classroomId: model.classroomId,
+  );
+}
+
+StudentModel studentEntityToModel(Student student) {
+  return StudentModel(
+    firstName: student.firstName,
+    lastName: student.lastName,
+    localId: student.id,
+    classroomId: student.classroomId,
+  );
+}
