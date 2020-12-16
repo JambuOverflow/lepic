f5d4f3a99466ed1aa940583d72d924ec7f073392
TakeshiYoshikawa <takeshi-y@hotmail.com>
2020-12-15 23:11:01 -0300

    Fix all tests
    Finally

--- back_end/tests/test_crud_text.py
+++ back_end/tests/test_crud_text.py
@@ -1,6 +1,6 @@
 from django.urls import reverse
 from rest_framework import status
-from api.models import Class
+from api.models import Class, User
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient, APITestCase
 
@@ -9,21 +9,17 @@ class TestCRUDText(APITestCase):
     user_url = reverse('list-and-create-users')
     text_url = reverse('create-text')
     auth_url = reverse('access-token')
-    auth_data = {
-            "username":"usertest",
-            "password":"123456"
-        }
 
     def setUp(self):
-        user_data = {
-            "first_name": "Test",
-            "last_name": "User",
-            "email": "user@test.com",
-            "username": "usertest",
-            "password": "123456",
-            "user_role": 1  # teacher
-        }
-        self.client.post(self.user_url, user_data, format='json')
+        self.email = "user@test.com"
+        self.username = "usertest"
+        self.password = "123456"
+        self.role = 1
+
+        self.class_data = {
+                'grade':7, 
+                'title':'Turma do Ideal'
+                }
 
 
     def test_create_text_without_authorization(self):
@@ -37,25 +33,19 @@ class TestCRUDText(APITestCase):
 
 
     def test_create_text_with_authorization(self):
-        response = self.client.post(self.auth_url, self.auth_data, format='json')
-        token = response.data['token']
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
-
-        #Create class
-        class_ = {
-                'grade':7, 
-                'title':'Turma do Ideal'
-                }
-        response = self.client.post(self.class_url, class_, format='json')
+        self.user = User.objects.create_user(username=self.username, password=self.password, email=self.email, role=self.role)
+        token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token {}'.format(token))
+        response = self.client.post(self.class_url, self.class_data, format='json')
         class_id = response.data['id']
-
-        #Create text
-        text = {
+        
+        text_data = {
             "title": "Trecho de O Alquimista",
             "body": "Lorem ipsum lorem 3",
             "_class":class_id
             }
-        response = self.client.post(self.text_url, text, format='json')
+
+        response = self.client.post(self.text_url, text_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
 
@@ -65,9 +55,9 @@ class TestCRUDText(APITestCase):
 
 
     def test_get_texts_with_authorization(self):
-        response = self.client.post(self.auth_url, self.auth_data, format='json')
-        token = response.data['token']
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
+        self.user = User.objects.create_user(username=self.username, password=self.password, email=self.email, role=self.role)
+        token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token {}'.format(token))
         response = self.client.get(self.text_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
