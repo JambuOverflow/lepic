00fe89642d00d8f6ecc49eeb554c56bab6af089c
VitorCantao <vitornovaes.cantao@gmail.com>
2020-12-17 13:28:09 -0300

    Update user form state

--- mobile/lib/features/user_management/presentation/bloc/user_form_state.dart
+++ mobile/lib/features/user_management/presentation/bloc/user_form_state.dart
@@ -1,6 +1,36 @@
 part of 'user_form_bloc.dart';
 
 class UserFormState extends Equatable {
+  final NameInput firstName;
+  final NameInput lastName;
+  final EmailInput email;
+  final PasswordInput password;
+  final FormzStatus status;
+
+  const UserFormState({
+    this.firstName = const NameInput.pure(),
+    this.lastName = const NameInput.pure(),
+    this.email = const EmailInput.pure(),
+    this.password = const PasswordInput.pure(),
+    this.status = FormzStatus.pure,
+  });
+
+  UserFormState copyWith({
+    final NameInput firstName,
+    final NameInput lastName,
+    final EmailInput email,
+    final PasswordInput password,
+    final FormzStatus status,
+  }) {
+    return UserFormState(
+      firstName: firstName ?? this.firstName,
+      lastName: lastName ?? this.lastName,
+      email: email ?? this.email,
+      password: password ?? this.password,
+      status: status ?? this.status,
+    );
+  }
+
   @override
-  List<Object> get props => [];
+  List<Object> get props => [firstName, lastName, email, password, status];
 }
