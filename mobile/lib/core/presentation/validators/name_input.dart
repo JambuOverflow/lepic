f68e6f71725940f504dc1d660f77cde4e0830d76
VitorCantao <vitornovaes.cantao@gmail.com>
2020-12-17 12:03:58 -0300

    Fix name input constructors

--- mobile/lib/core/presentation/validators/name_input.dart
+++ mobile/lib/core/presentation/validators/name_input.dart
@@ -3,8 +3,8 @@ import 'package:formz/formz.dart';
 enum NameValidationError { empty }
 
 class NameInput extends FormzInput<String, NameValidationError> {
-  const NameInput.pure({String value = ''}) : super.pure('');
-  const NameInput.dirty(String value) : super.dirty(value);
+  const NameInput.pure([String value = '']) : super.pure(value);
+  const NameInput.dirty([String value = '']) : super.dirty(value);
 
   @override
   NameValidationError validator(String value) {
