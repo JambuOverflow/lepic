2d670daf95dd38b1ca51447b79fcf1e768c6f403
Arouck <ppvitorr@gmail.com>
2020-12-18 17:32:39 -0300

    Create IsTeacherOrReadOnly permission So a user can only update, delete his students' accounts

--- back_end/api/permissions.py
+++ back_end/api/permissions.py
@@ -19,7 +19,14 @@ class IsOwner(permissions.BasePermission):
     """
     Custom permission to only allow owners of an object to edit it.
     """
-
     def has_object_permission(self, request, view, obj):
         # All permissions are only allowed to the owner of the object.
         return obj == request.user
+
+class IsTeacherOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow the teacher of a student to create or edit his/her account.
+    """
+    def has_object_permission(self, request, view, obj):
+        # Write permissions are only allowed to the owner of the object.
+        return obj._class.tutor == request.user
