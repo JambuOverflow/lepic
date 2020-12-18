f09c4b2c6b93e6b1947552b2bcb50ead8af6610f
Arouck <ppvitorr@gmail.com>
2020-12-18 14:48:52 -0300

    Update tests And create a test to create a student trying to overpass the permissions

--- back_end/tests/test_crud_student.py
+++ back_end/tests/test_crud_student.py
@@ -19,14 +19,26 @@ class TestCRUDStudent(APITestCase):
             "title": "Turma A",
             "grade": 3
         }
+        self.user = User.objects.create_user(username = 'pedro', password = 'pedro', email = 'pedro@ufpa.br', role = 0)
+        self.first_class = Class.objects.create(tutor=self.user, grade=7, title='Class A')
+        self.second_class = Class.objects.create(tutor=self.user, grade=9, title='Class B')
 
     def test_create_students(self):
-        self.user = User.objects.create_user(username="aian", email="aian@ufpa.br", password="123", role=1)
         token = Token.objects.create(user=self.user)
         self.client.credentials(HTTP_AUTHORIZATION='Token {}'.format(token))
         self.client.post(self.class_url, self.class_data, format='json')
         response = self.client.post(self.student_url_list_create, self.student_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Student.objects.count(), 1)
+
+    def test_create_student_from_another_class(self):
+        self.user_2 = User.objects.create_user(username = 'aian', password = 'shay', email = 'naia@ufpa.br', role = 0)
+        token = Token.objects.create(user=self.user_2)
+        self.client.credentials(HTTP_AUTHORIZATION='Token {}'.format(token))
+        response = self.client.post(self.student_url_list_create, self.student_data, format='json')
+        self.assertEqual(Student.objects.count(), 0)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.data['detail'], "Unable to add a student to a class that isn't yours")
 
     def test_list_students(self):
         self.user = User.objects.create_user(username="aian", email="aian@ufpa.br", password="123", role=1)
@@ -37,16 +49,10 @@ class TestCRUDStudent(APITestCase):
         self.assertEqual([], response.data)
     
     def test_update_put_students(self):
-        self.user = User.objects.create_user(username="aian", email="aian@ufpa.br", password="123", role=1)
         token = Token.objects.create(user=self.user)
         self.client.credentials(HTTP_AUTHORIZATION='Token {}'.format(token))
-        self.client.post(self.class_url, self.class_data, format='json')
-        self.client.post(self.student_url_list_create, self.student_data, format='json')
-        self.class_data_2 = {
-            "title": "Turma B",
-            "grade": 7
-        }
-        self.client.post(self.class_url, self.class_data_2, format='json')
+        Student.objects.create(_class=self.first_class, first_name='Arthur', last_name='Takeshi')
+        Class.objects.create(tutor=self.user, grade=10, title='Turma B')
         student_new_data = {
             'first_name': 'Aian',
             'last_name': 'Takeshi',
@@ -57,24 +63,20 @@ class TestCRUDStudent(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_update_patch_students(self):
-        self.user = User.objects.create_user(username="aian", email="aian@ufpa.br", password="123", role=1)
-        token = Token.objects.create(user=self.user)
-        self.client.credentials(HTTP_AUTHORIZATION='Token {}'.format(token))
-        self.client.post(self.class_url, self.class_data, format='json')
-        self.client.post(self.student_url_list_create, self.student_data, format='json')
         student_new_data = {
             'first_name': 'Aian'
         }
+        token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token {}'.format(token))
+        Student.objects.create(_class=self.first_class, first_name='Arthur', last_name='Takeshi')
         response = self.client.patch(self.student_url_update_delete, student_new_data, format='json')
         self.assertEqual(response.data['first_name'], student_new_data['first_name'])
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_delete_students(self):
-        self.user = User.objects.create_user(username="aian", email="aian@ufpa.br", password="123", role=1)
         token = Token.objects.create(user=self.user)
         self.client.credentials(HTTP_AUTHORIZATION='Token {}'.format(token))
-        self.client.post(self.class_url, self.class_data, format='json')
-        self.client.post(self.student_url_list_create, self.student_data, format='json')
+        Student.objects.create(_class=self.first_class, first_name='Arthur', last_name='Takeshi')
         response = self.client.delete(self.student_url_update_delete, self.student_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Student.objects.count(), 0)
