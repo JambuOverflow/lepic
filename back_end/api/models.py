18e61cfd54fbbc00ed73ddf167cc9891c4ea8b81
Arouck <ppvitorr@gmail.com>
2020-12-19 00:11:20 -0300

    Create AudioFile model

--- back_end/api/models.py
+++ back_end/api/models.py
@@ -48,3 +48,9 @@ class Student(models.Model):
 
     def __str__(self):
         return self.first_name + " " + self.last_name
+
+class AudioFile(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.ForeignKey(Text, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    file = models.FileField()
