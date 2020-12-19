48236d2f93efc4eb5c07658206ae8cca7eef1bc2
VitorCantao <vitornovaes.cantao@gmail.com>
2020-12-19 18:53:26 -0300

    Fix error in class injection container
    How it passed through Travis?

--- mobile/lib/class_injection_container.dart
+++ mobile/lib/class_injection_container.dart
@@ -8,6 +8,7 @@ import 'package:mobile/features/class_management/data/repositories/classroom_rep
 import 'package:mobile/features/class_management/domain/repositories/classroom_repository.dart';
 import 'package:mobile/features/class_management/domain/use_cases/create_classroom_use_case.dart';
 import 'package:mobile/features/class_management/domain/use_cases/delete_classroom_use_case.dart';
+import 'package:mobile/features/class_management/domain/use_cases/get_classrooms_use_case.dart';
 import 'package:mobile/features/class_management/domain/use_cases/update_classroom_use_case.dart';
 import 'package:mobile/features/class_management/presentation/bloc/class_bloc.dart';
 
@@ -17,18 +18,18 @@ final sl = GetIt.instance;
 
 void init() {
   sl.registerFactory(
-    () => ClassBloc(
-      updateClass: sl(),
-      deleteClass: sl(),
-      createNewClass: sl(),
-      //getClass: sl(),
+    () => ClassroomBloc(
+      updateClassroom: sl(),
+      deleteClassroom: sl(),
+      createNewClassroom: sl(),
+      getClassroom: sl(),
     ),
   );
 
   sl.registerLazySingleton(() => CreateClassroom(repository: sl()));
   sl.registerLazySingleton(() => UpdateClassroom(repository: sl()));
   sl.registerLazySingleton(() => DeleteClassroom(repository: sl()));
-  // sl.registerLazySingleton(() => GetClassrooms(repository: sl()));
+  sl.registerLazySingleton(() => GetClassrooms(repository: sl()));
 
   sl.registerLazySingleton<ClassroomRepository>(
     () => ClassroomRepositoryImpl(
