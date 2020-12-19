257e73e4d6a626b57f707d8990e1849ff5bcb956
VitorCantao <vitornovaes.cantao@gmail.com>
2020-12-19 12:15:10 -0300

    Update confirm password field

--- mobile/lib/features/user_management/presentation/widgets/input_fields/confirm_password_input_field.dart
+++ mobile/lib/features/user_management/presentation/widgets/input_fields/confirm_password_input_field.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../bloc/user_form_bloc.dart';
 
 class ConfirmPasswordInputField extends StatelessWidget {
+  final focusNode;
+
+  const ConfirmPasswordInputField({Key key, @required this.focusNode})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      obscureText: true,
-      decoration: InputDecoration(
-        border: OutlineInputBorder(),
-        labelText: 'Confirm password',
-      ),
+    return BlocBuilder<UserFormBloc, UserFormState>(
+      builder: (context, state) {
+        return TextFormField(
+          initialValue: state.confirmPassword.value,
+          focusNode: focusNode,
+          obscureText: true,
+          decoration: InputDecoration(
+            prefixIcon: const Icon(Icons.lock_outline),
+            border: OutlineInputBorder(),
+            labelText: 'Confirm Password',
+            errorText: state.confirmPassword.invalid
+                ? 'Those passwords didn\'t match. Please, try again.'
+                : null,
+          ),
+          keyboardType: TextInputType.text,
+          onChanged: (value) => context
+              .read<UserFormBloc>()
+              .add(ConfirmPasswordChanged(confirmPassword: value)),
+          textInputAction: TextInputAction.done,
+        );
+      },
     );
   }
 }
