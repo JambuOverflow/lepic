da655f4b8f7881a8162350209930ffc743ee84cf
VitorCantao <vitornovaes.cantao@gmail.com>
2020-12-19 22:14:24 -0300

    Refactor login page and improve visuals

--- mobile/lib/features/user_management/presentation/pages/login.dart
+++ mobile/lib/features/user_management/presentation/pages/login.dart
@@ -11,116 +11,158 @@ class _LoginState extends State<Login> {
   TextEditingController nameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
-  UserBloc _userBloc;
   @override
   Widget build(BuildContext context) {
-    _userBloc = BlocProvider.of<UserBloc>(context);
     return Scaffold(
       body: Padding(
-        padding: EdgeInsets.all(8.0),
+        padding: EdgeInsets.fromLTRB(16, 16, 16, 0),
         child: ListView(
           children: <Widget>[
-            Container(
-                alignment: Alignment.center,
-                child: Text(
-                  'Lepic',
-                  style: TextStyle(fontSize: 20),
-                )),
-            Container(
-              padding: EdgeInsets.all(8.0),
-              child: TextField(
-                controller: nameController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'E-mail',
-                ),
+            SizedBox(height: 32),
+            Image.asset(
+              "assets/images/JambuOverflow.png",
+              width: MediaQuery.of(context).size.width / 3,
+              height: MediaQuery.of(context).size.width / 3,
+            ),
+            SizedBox(height: 8),
+            Center(
+              child: Text(
+                'Lepic',
+                style: Theme.of(context).textTheme.headline4,
               ),
             ),
-            Container(
-              padding: EdgeInsets.all(8.0),
-              child: TextField(
-                obscureText: true,
-                controller: passwordController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                ),
-              ),
-            ),
-            BlocListener<UserBloc, UserState>(
-              listener: (context, state) {
-                if (state is LoggedIn) {
-                  print('logged in');
-                  Navigator.of(context).pushNamed(
-                    '/home',
-                  );
-                } else if (state is Error) {
-                  Scaffold.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text(state.message),
-                    ),
-                  );
-                }
-              },
-              child:
-                  BlocBuilder<UserBloc, UserState>(builder: (context, state) {
-                return Container(
-                  height: 80,
-                  padding: EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 16.0),
-                  child: RaisedButton(
-                    child: Text(
-                      'Login',
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    onPressed: () {
-                      print(nameController.text);
-                      print(passwordController.text);
-                      _userBloc.add(LoggingUserEvent(
-                        nameController.text,
-                        passwordController.text,
-                      ));
-                    },
-                  ),
-                );
-              }),
-            ),
-            Container(
-              height: 80,
-              padding: EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 16.0),
-              child: OutlineButton(
-                child: Text(
-                  'Login as guest',
-                  style: TextStyle(fontSize: 16),
-                ),
-                onPressed: () {
-                  print("opening guest screen");
-                  Navigator.of(context).pushNamed(
-                    '/guest',
-                  );
-                },
-              ),
-            ),
-            Container(
-                padding: EdgeInsets.all(8.0),
-                child: FlatButton(
-                  child: Text(
-                    'Sign up',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  onPressed: () {
-                    print("Opening signup screen...");
-                    Navigator.of(context).pushNamed(
-                      '/signup',
-                    );
-                  },
-                ))
+            SizedBox(height: 16),
+            UserLoginInputField(nameController: nameController),
+            SizedBox(height: 8),
+            PasswordLoginInputField(passwordController: passwordController),
+            LoginButton(),
+            Row(
+              children: [
+                Expanded(child: GuestLoginButton()),
+                SizedBox(width: 16),
+                Expanded(child: SignupButton()),
+              ],
+            )
           ],
         ),
       ),
     );
   }
-  /*void submitLogin(BuildContext context, String name, String password){
-    final
-    _userBloc
-  }*/
+}
+
+class UserLoginInputField extends StatelessWidget {
+  const UserLoginInputField({
+    Key key,
+    @required this.nameController,
+  }) : super(key: key);
+
+  final TextEditingController nameController;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: nameController,
+      decoration: InputDecoration(
+        border: UnderlineInputBorder(),
+        labelText: 'E-mail',
+        prefixIcon: Icon(Icons.perm_identity_rounded),
+      ),
+    );
+  }
+}
+
+class PasswordLoginInputField extends StatelessWidget {
+  const PasswordLoginInputField({
+    Key key,
+    @required this.passwordController,
+  }) : super(key: key);
+
+  final TextEditingController passwordController;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      obscureText: true,
+      controller: passwordController,
+      decoration: InputDecoration(
+        border: UnderlineInputBorder(),
+        labelText: 'Password',
+        prefixIcon: Icon(Icons.lock_rounded),
+      ),
+    );
+  }
+}
+
+class LoginButton extends StatelessWidget {
+  const LoginButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<UserBloc, UserState>(
+      listener: (context, state) {
+        if (state is LoggedIn) {
+          Navigator.of(context).pushNamed(
+            '/home',
+          );
+        } else if (state is Error) {
+          Scaffold.of(context).showSnackBar(
+            SnackBar(
+              content: Text(state.message),
+            ),
+          );
+        }
+      },
+      builder: (context, state) {
+        return Container(
+          height: 80,
+          padding: EdgeInsets.symmetric(vertical: 16.0),
+          child: RaisedButton(
+            child: Text(
+              'Login',
+              style: TextStyle(fontSize: 16),
+            ),
+            onPressed: () {
+              throw UnimplementedError();
+            },
+          ),
+        );
+      },
+    );
+  }
+}
+
+class SignupButton extends StatelessWidget {
+  const SignupButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      child: Text(
+        'Sign up',
+        style: TextStyle(fontSize: 16),
+      ),
+      onPressed: () => Navigator.of(context).pushNamed('/signup'),
+    );
+  }
+}
+
+class GuestLoginButton extends StatelessWidget {
+  const GuestLoginButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      child: Text(
+        'Login as guest',
+        style: TextStyle(fontSize: 16),
+      ),
+      onPressed: () => Navigator.of(context).pushNamed('/guest'),
+    );
+  }
 }
