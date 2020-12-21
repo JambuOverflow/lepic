37dd88b99a66e5d5b71320a650d1d4b899f11831
VitorCantao <vitornovaes.cantao@gmail.com>
2020-12-21 01:39:01 -0300

    Update login button

--- mobile/lib/features/user_management/presentation/widgets/login_button.dart
+++ mobile/lib/features/user_management/presentation/widgets/login_button.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
-import '../bloc/user_bloc.dart';
+import 'package:formz/formz.dart';
+import 'package:mobile/features/user_management/presentation/bloc/login_form_bloc.dart';
 
 class LoginButton extends StatelessWidget {
   const LoginButton({
@@ -10,35 +10,48 @@ class LoginButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<UserBloc, UserState>(
-      listener: (context, state) {
-        if (state is LoggedIn) {
-          Navigator.of(context).pushNamed(
-            '/home',
-          );
-        } else if (state is Error) {
-          Scaffold.of(context).showSnackBar(
-            SnackBar(
-              content: Text(state.message),
-            ),
-          );
-        }
-      },
+    return BlocConsumer<LoginFormBloc, LoginFormState>(
       builder: (context, state) {
         return Container(
           height: 80,
           padding: EdgeInsets.symmetric(vertical: 16.0),
           child: RaisedButton(
-            child: Text(
-              'Login',
-              style: TextStyle(fontSize: 16),
-            ),
-            onPressed: () {
-              throw UnimplementedError();
-            },
-          ),
+              child: _loadingOrTextBasedOn(state),
+              onPressed: () {
+                FocusScope.of(context).unfocus();
+                BlocProvider.of<LoginFormBloc>(context).add(FormSubmitted());
+              }),
         );
       },
+      listener: (context, state) {
+        if (state.status == FormzStatus.submissionSuccess) {
+          Navigator.of(context).pushNamed('/home');
+        } else if (state.status == FormzStatus.submissionFailure) {
+          _showServerFailureSnackBar(context);
+        }
+      },
+    );
+  }
+
+  Widget _loadingOrTextBasedOn(LoginFormState state) {
+    if (state.status == FormzStatus.submissionInProgress)
+      return CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+      );
+    else
+      return Text(
+        'Login',
+        style: TextStyle(fontSize: 16),
+      );
+  }
+
+  void _showServerFailureSnackBar(BuildContext context) {
+    Scaffold.of(context).hideCurrentSnackBar();
+    Scaffold.of(context).showSnackBar(
+      SnackBar(
+        behavior: SnackBarBehavior.floating,
+        content: Text('Could not reach server'),
+      ),
     );
   }
 }
