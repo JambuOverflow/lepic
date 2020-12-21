39d3ec61e537edfbe7f96c2cff6bf63c00afadb1
VitorCantao <vitornovaes.cantao@gmail.com>
2020-12-21 01:35:22 -0300

    Add login form bloc

--- mobile/lib/injection_container.dart
+++ mobile/lib/injection_container.dart
@@ -2,6 +2,7 @@ import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mobile/features/user_management/domain/use_cases/login.dart';
+import 'package:mobile/features/user_management/presentation/bloc/login_form_bloc.dart';
 import 'package:mobile/features/user_management/presentation/bloc/signup_form_bloc.dart';
 import 'core/data/database.dart';
 import 'features/user_management/data/data_sources/user_local_data_source.dart';
@@ -27,6 +28,7 @@ void setUpLocator() {
   );
 
   getIt.registerFactory(() => SignupFormBloc(createNewUser: getIt()));
+  getIt.registerFactory(() => LoginFormBloc(loginCase: getIt()));
 
   getIt.registerLazySingleton(() => CreateNewUserCase(repository: getIt()));
   getIt.registerLazySingleton(() => LoginCase(repository: getIt()));
