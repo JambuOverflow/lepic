b7030c92bbb32eb922fe1869334ad07724f286bf
VitorCantao <vitornovaes.cantao@gmail.com>
2020-12-21 01:39:14 -0300

    Update login page

--- mobile/lib/features/user_management/presentation/pages/login.dart
+++ mobile/lib/features/user_management/presentation/pages/login.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:moor/moor.dart';
 
+import '../bloc/login_form_bloc.dart';
+import '../widgets/invalid_credentials_warning.dart';
 import '../widgets/input_fields/user_login_input_field.dart';
 import '../widgets/input_fields/password_login_input_field.dart';
 import '../widgets/login_button.dart';
@@ -12,44 +16,49 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
-  TextEditingController nameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.fromLTRB(16, 16, 16, 0),
-        child: ListView(
-          children: <Widget>[
-            SizedBox(height: 32),
-            Image.asset(
-              "assets/images/JambuOverflow.png",
-              width: MediaQuery.of(context).size.width / 3,
-              height: MediaQuery.of(context).size.width / 3,
-            ),
-            SizedBox(height: 8),
-            Center(
-              child: Text(
-                'Lepic',
-                style: Theme.of(context).textTheme.headline4,
-              ),
-            ),
-            SizedBox(height: 16),
-            UserLoginInputField(nameController: nameController),
-            SizedBox(height: 8),
-            PasswordLoginInputField(passwordController: passwordController),
-            LoginButton(),
-            Row(
-              children: [
-                Expanded(child: GuestLoginButton()),
-                SizedBox(width: 16),
-                Expanded(child: SignupButton()),
+    return BlocBuilder<LoginFormBloc, LoginFormState>(
+        builder: (context, state) {
+      return Scaffold(
+        body: Form(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(16, 16, 16, 0),
+            child: ListView(
+              children: <Widget>[
+                SizedBox(height: 32),
+                Image.asset(
+                  "assets/images/JambuOverflow.png",
+                  width: MediaQuery.of(context).size.width / 3,
+                  height: MediaQuery.of(context).size.width / 3,
+                ),
+                SizedBox(height: 8),
+                Center(
+                  child: Text(
+                    'Lepic',
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                ),
+                SizedBox(height: 16),
+                UserLoginInputField(),
+                SizedBox(height: 8),
+                PasswordLoginInputField(),
+                SizedBox(height: 12),
+                InvalidCredentialsWarning(isVisible: state.isCredentialInvalid),
+                SizedBox(height: 16),
+                LoginButton(),
+                Row(
+                  children: [
+                    Expanded(child: GuestLoginButton()),
+                    SizedBox(width: 16),
+                    Expanded(child: SignupButton()),
+                  ],
+                )
               ],
-            )
-          ],
+            ),
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
