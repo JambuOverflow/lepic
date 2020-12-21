83bfbf2c0e4ab5c50cffa6fea8bf1df81d303ed1
VitorCantao <vitornovaes.cantao@gmail.com>
2020-12-21 01:38:38 -0300

    Update user login input field

--- mobile/lib/features/user_management/presentation/widgets/input_fields/user_login_input_field.dart
+++ mobile/lib/features/user_management/presentation/widgets/input_fields/user_login_input_field.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../bloc/login_form_bloc.dart';
 
 class UserLoginInputField extends StatelessWidget {
   const UserLoginInputField({
     Key key,
-    @required this.nameController,
   }) : super(key: key);
-
-  final TextEditingController nameController;
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      controller: nameController,
-      decoration: InputDecoration(
-        border: UnderlineInputBorder(),
-        labelText: 'E-mail',
-        prefixIcon: Icon(Icons.perm_identity_rounded),
-      ),
+    return BlocBuilder<LoginFormBloc, LoginFormState>(
+      builder: (context, state) {
+        return TextFormField(
+          decoration: InputDecoration(
+            border: UnderlineInputBorder(),
+            labelText: 'E-mail',
+            prefixIcon: Icon(Icons.perm_identity_rounded),
+            errorText: state.email.invalid ? 'Invalid email' : null,
+          ),
+          onChanged: (value) => BlocProvider.of<LoginFormBloc>(context)
+              .add(EmailChanged(email: value)),
+        );
+      },
     );
   }
 }
