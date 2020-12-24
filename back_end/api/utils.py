4ffd0425dd53f5eb52f21a2dc6c1ddf205cdb1fd
Arouck <ppvitorr@gmail.com>
2020-12-24 11:17:22 -0300

    Create EmailThread util

---
+++ back_end/api/utils.py
@@ -0,0 +1,9 @@
+import threading
+
+class EmailThread(threading.Thread):
+    def __init__(self, email):
+        self.email = email
+        threading.Thread.__init__(self)
+
+    def run(self):
+        self.email.send(fail_silently=False)
