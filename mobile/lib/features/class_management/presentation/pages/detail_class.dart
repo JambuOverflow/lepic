ff060611e1a04e19aa2002c942d52f9d5254701a
VitorCantao <vitornovaes.cantao@gmail.com>
2020-12-26 10:59:43 -0300

    Refactor list class

--- mobile/lib/features/class_management/presentation/pages/detail_class.dart
+++ mobile/lib/features/class_management/presentation/pages/detail_class.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import '../../domain/entities/classroom.dart';
 
 enum MenuOption { Assign, Edit, Remove }
 
--- mobile/lib/features/class_management/presentation/pages/list_class.dart
+++ mobile/lib/features/class_management/presentation/pages/list_class.dart
@@ -12,58 +12,59 @@ class ShowClasses extends StatefulWidget {
 class _ShowClassesState extends State<ShowClasses> {
   TextEditingController tutorIdController = TextEditingController();
   TextEditingController nameController = TextEditingController();
-  ClassroomBloc _classBloc;
+
   @override
   Widget build(BuildContext context) {
-    _classBloc = BlocProvider.of<ClassroomBloc>(context); //aqui começa o erro
     return Scaffold(
-        appBar: AppBar(title: Text('Classes')),
-        drawer: DrawerOverlay(),
-        floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
-            onPressed: () {
-              Navigator.of(context).pushNamed(
-                '/add_class',
-              );
-            }),
-        body: Container(
-            child: ListView(children: <Widget>[
-          RaisedButton(child: Text('Search Class'), onPressed: null),
-          BlocListener<ClassroomBloc, ClassroomState>(
+      appBar: AppBar(title: Text('Classes')),
+      drawer: DrawerOverlay(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.of(context).pushNamed(
+            '/add_class',
+          );
+        },
+      ),
+      body: Container(
+        child: ListView(
+          children: <Widget>[
+            RaisedButton(child: Text('Search Class'), onPressed: null),
+            BlocConsumer<ClassroomBloc, ClassroomState>(
               listener: (context, state) {
-            if (state is Error) {
-              Scaffold.of(context).showSnackBar(
-                SnackBar(
-                  content: Text(state.message),
-                ),
-              );
-            }
-          }, child: BlocBuilder<ClassroomBloc, ClassroomState>(
-                  builder: (context, state) {
-            return Container(
-                height: 80,
-                padding: EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 16.0),
-                child: ListTile(
+                if (state is Error) {
+                  Scaffold.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(state.message),
+                    ),
+                  );
+                }
+              },
+              builder: (context, state) {
+                return Container(
+                  height: 80,
+                  padding: EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 16.0),
+                  child: ListTile(
                     title: Text('Class Name'),
                     subtitle: Text('Class Grade'),
                     trailing: IconButton(
-                        icon: Icon(Icons.arrow_forward),
-                        onPressed: () {
-                          print("Redirecting to detailed class");
-                          print("name: " + nameController.text);
-                          print("tutorID: " + tutorIdController.text); //as int
-                          Navigator.of(context).pushNamed(
-                            '/detail_class',
-                          );
-                          /*GetClassEvent(
-                          tutorIdController.text as int,
-                          nameController.text,
-                        );*/
-                        })));
-          }))
-        ])));
+                      icon: Icon(Icons.arrow_forward),
+                      onPressed: () {
+                        print("Redirecting to detailed class");
+                        print("name: " + nameController.text);
+                        print("tutorID: " + tutorIdController.text); //as int
+                        Navigator.of(context).pushNamed(
+                          '/detail_class',
+                        );
+                      },
+                    ),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
-
-//card widget pra cada turma
-
 }
