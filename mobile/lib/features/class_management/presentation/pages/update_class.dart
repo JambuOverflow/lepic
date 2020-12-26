52183e840c1fcd3b85ef871693bc3bef0e403fe8
VitorCantao <vitornovaes.cantao@gmail.com>
2020-12-26 11:05:51 -0300

    Refactor update class

--- mobile/lib/features/class_management/presentation/pages/update_class.dart
+++ mobile/lib/features/class_management/presentation/pages/update_class.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobile/features/class_management/presentation/widgets/classroom_form_field.dart';
 
 import '../bloc/classroom_bloc.dart';
 
@@ -13,99 +14,65 @@ class _UpdateClassState extends State<UpdateClass> {
   TextEditingController _gradeController = TextEditingController();
   TextEditingController _tutorIdController = TextEditingController();
   TextEditingController _levelSelected = TextEditingController();
-  ClassroomBloc _classBloc;
 
   @override
   Widget build(BuildContext context) {
-    _classBloc = BlocProvider.of<ClassroomBloc>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Update class'),
       ),
       body: Padding(
         padding: EdgeInsets.all(8.0),
-        child: ListView(children: <Widget>[
-          ClasroomForm(
-            label: 'Current Name',
-            textController: _nameController,
-          ),
-          ClasroomForm(
-            label: 'Current Grade',
-            textController: _gradeController,
-            numeric: true,
-          ),
-          ClasroomForm(
-            label: 'Current Tutor',
-            textController: _tutorIdController,
-            numeric: true,
-          ),
-          ClasroomForm(
-            //This does not make much sense to be together
-            label: 'Current Id - for tests',
-            textController: _levelSelected,
-            numeric: true,
-          ),
-          BlocListener<ClassroomBloc, ClassroomState>(
-            listener: (context, state) {
-              if (state is Error) {
-                Scaffold.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text(state.message),
+        child: ListView(
+          children: <Widget>[
+            ClasroomFormField(
+              label: 'Current Name',
+              textController: _nameController,
+            ),
+            ClasroomFormField(
+              label: 'Current Grade',
+              textController: _gradeController,
+              numeric: true,
+            ),
+            ClasroomFormField(
+              label: 'Current Tutor',
+              textController: _tutorIdController,
+              numeric: true,
+            ),
+            ClasroomFormField(
+              //This does not make much sense to be together
+              label: 'Current Id - for tests',
+              textController: _levelSelected,
+              numeric: true,
+            ),
+            BlocConsumer<ClassroomBloc, ClassroomState>(
+              listener: (context, state) {
+                if (state is Error) {
+                  Scaffold.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(state.message),
+                    ),
+                  );
+                } else if (state is Error) {
+                  Navigator.pop(context);
+                }
+              },
+              builder: (context, state) {
+                return Container(
+                  height: 80,
+                  padding: EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 16.0),
+                  child: RaisedButton(
+                    child: Text(
+                      'Update',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    onPressed: () {},
                   ),
                 );
-              } else if (state is Error) {
-                Navigator.pop(context);
-              }
-            },
-            child: BlocBuilder<ClassroomBloc, ClassroomState>(
-                builder: (context, state) {
-              return Container(
-                height: 80,
-                padding: EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 16.0),
-                child: RaisedButton(
-                  child: Text(
-                    'Update',
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  onPressed: () {},
-                ),
-              );
-            }),
-          ),
-        ]),
-      ),
-    );
-  }
-}
-
-class ClasroomForm extends StatefulWidget {
-  final TextEditingController textController;
-  final String label;
-  final bool numeric;
-
-  ClasroomForm({
-    this.textController,
-    this.label,
-    this.numeric,
-  });
-
-  @override
-  _ClasroomFormState createState() => _ClasroomFormState();
-}
-
-class _ClasroomFormState extends State<ClasroomForm> {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(8.0),
-      child: TextField(
-        controller: widget.textController,
-        decoration: InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: widget.label,
+              },
+            ),
+          ],
         ),
-        keyboardType:
-            widget.numeric != null ? TextInputType.number : TextInputType.text,
       ),
     );
   }
