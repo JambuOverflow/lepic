1c13891f27f879c034e38d8ca6a43faff97f3452
renan-cunha <renancunhafonseca@gmail.com>
2020-12-29 17:19:08 -0300

    Add schoolId in classroom

--- mobile/lib/features/class_management/domain/entities/classroom.dart
+++ mobile/lib/features/class_management/domain/entities/classroom.dart
@@ -2,9 +2,9 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 
-
 class Classroom extends Equatable {
   final int tutorId;
+  final int schoolId;
   final int id;
   final int grade;
   final String name;
@@ -13,8 +13,9 @@ class Classroom extends Equatable {
       {@required this.tutorId,
       @required this.grade,
       @required this.name,
+      @required this.schoolId,
       this.id});
 
   @override
-  List<Object> get props => [tutorId, grade, name, id];
+  List<Object> get props => [tutorId, grade, name, id, schoolId];
 }
