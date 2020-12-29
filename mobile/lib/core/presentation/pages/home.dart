3fe6623f632fb59a5e2da3269a2ad47f150e933c
VitorCantao <vitornovaes.cantao@gmail.com>
2020-12-29 18:39:28 -0300

    Modify home page to show user greeting for testing purposes

--- mobile/lib/core/presentation/pages/home.dart
+++ mobile/lib/core/presentation/pages/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobile/features/user_management/domain/entities/user.dart';
+import 'package:mobile/features/user_management/presentation/bloc/auth_bloc.dart';
 
 import '../widgets/drawer_overlay.dart';
 
@@ -10,9 +13,21 @@ class HomeScreen extends StatefulWidget {
 class _MyStatefulWidgetState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    final User user = BlocProvider.of<AuthBloc>(context).state.user;
+
     return Scaffold(
       appBar: AppBar(title: Text('Home')),
-      body: Text("Home has no content, may become some sort of catalog"),
+      body: Center(
+        child: Text(
+          "Welcome, " +
+              user.firstName.toUpperCase() +
+              '\n\n' +
+              'Nothing here... for now.',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              fontSize: Theme.of(context).textTheme.headline6.fontSize),
+        ),
+      ),
       drawer: DrawerOverlay(),
     );
   }
