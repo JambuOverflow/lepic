af149f8ad1b97cdb26bc090b0c3c88a6d1ecf47b
VitorCantao <vitornovaes.cantao@gmail.com>
2020-12-30 10:40:28 -0300

    Clean route generator

--- mobile/lib/core/presentation/pages/route_generator.dart
+++ mobile/lib/core/presentation/pages/route_generator.dart
@@ -1,26 +1,22 @@
-//This file defines all the routing logic so it won't be anywhere else in the code
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
-import 'package:mobile/features/class_management/presentation/pages/classroom_detail_page.dart';
-import 'package:mobile/features/class_management/presentation/pages/classrooms_page.dart';
-import 'package:mobile/features/class_management/presentation/pages/classroom_creation_page.dart';
-import 'package:mobile/features/class_management/presentation/pages/classroom_update_page.dart';
-import 'package:mobile/features/student_management/domain/entities/student.dart';
-import 'package:mobile/features/student_management/presentation/bloc/student_bloc.dart';
-import 'package:mobile/features/student_management/presentation/pages/create_student_page.dart';
-import 'package:mobile/features/student_management/presentation/pages/student_detail_page.dart';
-import 'package:mobile/features/student_management/presentation/pages/students_page.dart';
-import 'package:mobile/features/text_management/presentation/pages/text_creation_page.dart';
-import 'package:mobile/features/text_management/presentation/pages/texts_page.dart';
-import 'package:mobile/features/user_management/presentation/bloc/login_form_bloc.dart';
-import 'package:mobile/features/user_management/presentation/pages/guest.dart';
 
-import 'package:mobile/features/user_management/presentation/bloc/signup_form_bloc.dart';
-import 'package:mobile/features/user_management/presentation/pages/signup_success.dart';
-import 'package:mobile/features/user_management/presentation/pages/login.dart';
-import 'package:mobile/features/user_management/presentation/pages/signup.dart';
-import 'package:mobile/features/user_management/presentation/pages/update_user.dart';
+import '../../../features/class_management/presentation/pages/classroom_detail_page.dart';
+import '../../../features/class_management/presentation/pages/classrooms_page.dart';
+import '../../../features/class_management/presentation/pages/classroom_creation_page.dart';
+import '../../../features/class_management/presentation/pages/classroom_update_page.dart';
+import '../../../features/student_management/presentation/pages/create_student_page.dart';
+import '../../../features/student_management/presentation/pages/students_page.dart';
+import '../../../features/text_management/presentation/pages/text_creation_page.dart';
+import '../../../features/text_management/presentation/pages/texts_page.dart';
+import '../../../features/user_management/presentation/bloc/login_form_bloc.dart';
+import '../../../features/user_management/presentation/pages/guest.dart';
+import '../../../features/user_management/presentation/bloc/signup_form_bloc.dart';
+import '../../../features/user_management/presentation/pages/signup_success.dart';
+import '../../../features/user_management/presentation/pages/login.dart';
+import '../../../features/user_management/presentation/pages/signup.dart';
+import '../../../features/user_management/presentation/pages/update_user.dart';
 import 'home.dart';
 
 class RouteGenerator {
@@ -34,9 +30,7 @@ class RouteGenerator {
           ),
         );
       case '/home':
-        return MaterialPageRoute(
-          builder: (_) => HomeScreen(),
-        );
+        return MaterialPageRoute(builder: (_) => HomeScreen());
       case '/signup':
         return MaterialPageRoute(
           builder: (_) => BlocProvider(
@@ -45,49 +39,28 @@ class RouteGenerator {
           ),
         );
       case '/guest':
-        return MaterialPageRoute(
-          builder: (_) => Guest(),
-        );
+        return MaterialPageRoute(builder: (_) => Guest());
       case '/update_user':
-        return MaterialPageRoute(
-          builder: (_) => UpdateUser(),
-        );
+        return MaterialPageRoute(builder: (_) => UpdateUser());
       case '/signup_success':
         return MaterialPageRoute(builder: (_) => SignUpSuccess());
       case '/list_students':
-        return MaterialPageRoute(
-          builder: (_) => StudentsPage(),
-        );
+        return MaterialPageRoute(builder: (_) => StudentsPage());
       case '/add_student':
-        return MaterialPageRoute(
-          builder: (_) => AddStudent(),
-        );
+        return MaterialPageRoute(builder: (_) => AddStudent());
       case '/list_classes':
-        return MaterialPageRoute(
-          builder: (_) => ClassroomsPage(),
-        );
+        return MaterialPageRoute(builder: (_) => ClassroomsPage());
       case '/add_class':
-        return MaterialPageRoute(
-          builder: (_) => ClassroomCreationPage(),
-        );
+        return MaterialPageRoute(builder: (_) => ClassroomCreationPage());
       case '/list_texts':
-        return MaterialPageRoute(
-          builder: (_) => TextsPage(),
-        );
+        return MaterialPageRoute(builder: (_) => TextsPage());
       case '/add_text':
-        return MaterialPageRoute(
-          builder: (_) => TextCreationPage(),
-        );
+        return MaterialPageRoute(builder: (_) => TextCreationPage());
       case '/detail_class':
-        return MaterialPageRoute(
-          builder: (_) =>
-              ClassroomDetailPage(/*_schoolClass*/), //receives arguments
-        );
+        return MaterialPageRoute(builder: (_) => ClassroomDetailPage());
 
       case '/update_class':
-        return MaterialPageRoute(
-          builder: (_) => ClassroomUpdatePage(),
-        );
+        return MaterialPageRoute(builder: (_) => ClassroomUpdatePage());
 
       default:
         return _errorRoute();
@@ -95,14 +68,13 @@ class RouteGenerator {
   }
 
   static Route<dynamic> _errorRoute() {
-    return MaterialPageRoute(builder: (_) {
-      return Scaffold(
-          appBar: AppBar(
-            title: Text('Error'),
-          ),
-          body: Center(
-            child: Text('Page not Found'),
-          ));
-    });
+    return MaterialPageRoute(
+      builder: (_) {
+        return Scaffold(
+          appBar: AppBar(title: Text('Error')),
+          body: Center(child: Text('Page not Found')),
+        );
+      },
+    );
   }
 }
