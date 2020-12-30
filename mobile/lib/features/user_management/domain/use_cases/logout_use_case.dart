f68da433054fe6405a76976658e9d6a9154994e3
VitorCantao <vitornovaes.cantao@gmail.com>
2020-12-29 23:36:19 -0300

    Fix logout use case

--- mobile/lib/features/user_management/domain/use_cases/logout_use_case.dart
+++ mobile/lib/features/user_management/domain/use_cases/logout_use_case.dart
@@ -2,17 +2,14 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter/foundation.dart';
 
 import '../../../../core/error/failures.dart';
-import '../../../../core/network/response.dart';
 import '../../../../core/use_cases/use_case.dart';
 import '../repositories/user_repository.dart';
-import 'user_params.dart';
 
-class LogoutCase implements UseCase<Response, UserParams> {
+class LogoutCase implements UseCase<void, NoParams> {
   final UserRepository repository;
 
   LogoutCase({@required this.repository});
 
   @override
-  Future<Either<Failure, Response>> call(NoParams) async =>
-      await repository.logout();
+  Future<Either<Failure, void>> call(params) async => await repository.logout();
 }
