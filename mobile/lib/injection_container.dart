293e725ae9281006932090c8b7601c17a7f4f748
VitorCantao <vitornovaes.cantao@gmail.com>
2020-12-29 23:56:37 -0300

    Add logout to injection container

--- mobile/lib/injection_container.dart
+++ mobile/lib/injection_container.dart
@@ -2,6 +2,7 @@ import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mobile/features/user_management/domain/use_cases/login.dart';
+import 'package:mobile/features/user_management/domain/use_cases/logout_use_case.dart';
 import 'package:mobile/features/user_management/domain/use_cases/retrieve_token_use_case.dart';
 import 'package:mobile/features/user_management/presentation/bloc/auth_bloc.dart';
 import 'package:mobile/features/user_management/presentation/bloc/login_form_bloc.dart';
@@ -28,13 +29,19 @@ void setUpLocator() {
   si.init();
   ti.init();
 
-  getIt.registerLazySingleton(() => AuthBloc(getLoggedInUserCase: getIt()));
+  getIt.registerLazySingleton(
+    () => AuthBloc(
+      getLoggedInUserCase: getIt(),
+      logoutCase: getIt(),
+    ),
+  );
 
   getIt.registerFactory(() => SignupFormBloc(createNewUser: getIt()));
   getIt.registerFactory(() => LoginFormBloc(loginCase: getIt()));
 
   getIt.registerLazySingleton(() => CreateNewUserCase(repository: getIt()));
   getIt.registerLazySingleton(() => LoginCase(repository: getIt()));
+  getIt.registerLazySingleton(() => LogoutCase(repository: getIt()));
   getIt.registerLazySingleton(() => UpdateUserCase(repository: getIt()));
   getIt.registerLazySingleton(() => GetLoggedInUserCase(repository: getIt()));
   getIt.registerLazySingleton(() => RetrieveTokenUseCase(repository: getIt()));
