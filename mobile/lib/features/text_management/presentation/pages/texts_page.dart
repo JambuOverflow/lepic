ab433a2de1b4f7bf868927258bfccef244047550
VitorCantao <vitornovaes.cantao@gmail.com>
2021-01-02 18:57:18 -0300

    Update texts page with bloc consumer

--- mobile/lib/features/text_management/presentation/pages/texts_page.dart
+++ mobile/lib/features/text_management/presentation/pages/texts_page.dart
@@ -14,16 +14,35 @@ class TextsPage extends StatefulWidget {
 
 class _TextsPageState extends State<TextsPage> {
   @override
+  void initState() {
+    super.initState();
+    BlocProvider.of<TextBloc>(context).add(GetTextsEvent());
+  }
+
+  @override
   Widget build(BuildContext context) {
     final _bloc = BlocProvider.of<TextBloc>(context);
 
     return Scaffold(
       appBar: BackgroundAppBar(title: 'Texts'),
-      body: ListView.builder(
-        itemCount: _bloc.texts.length,
-        itemBuilder: (context, index) {
-          final text = _bloc.texts[index];
-          return TextItem(text);
+      body: BlocConsumer<TextBloc, TextState>(
+        builder: (context, state) {
+          if (state is TextsLoaded) {
+            return ListView.builder(
+              itemCount: _bloc.texts.length,
+              itemBuilder: (context, index) {
+                final text = _bloc.texts[index];
+                return TextItem(text);
+              },
+            );
+          } else
+            return CircularProgressIndicator();
+        },
+        listener: (context, state) {
+          if (state is Error)
+            Scaffold.of(context).showSnackBar(
+              SnackBar(content: Text(state.message)),
+            );
         },
       ),
       floatingActionButton: FloatingActionButton(
