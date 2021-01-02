141a2f61cf81a502fb44066099be876709da29f0
VitorCantao <vitornovaes.cantao@gmail.com>
2021-01-02 10:32:46 -0300

    Update classroom detail with app bar

--- mobile/lib/features/class_management/presentation/pages/classroom_detail_page.dart
+++ mobile/lib/features/class_management/presentation/pages/classroom_detail_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/core/presentation/widgets/flight_shuttle_builder.dart';
-import 'package:mobile/features/class_management/domain/entities/classroom.dart';
+
+import '../../domain/entities/classroom.dart';
+import '../../../student_management/presentation/pages/students_page.dart';
+import '../../../text_management/presentation/pages/texts_page.dart';
+import '../../../../core/presentation/widgets/flight_shuttle_builder.dart';
 
 enum MenuOption { Assign, Edit, Remove }
 
@@ -12,83 +15,36 @@ class ClassroomDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text('Class Details'),
-      //   actions: <Widget>[
-      //     PopupMenuButton<MenuOption>(
-      //       onSelected: (MenuOption result) {
-      //         if (result == MenuOption.Assign) {
-      //           Navigator.of(context).pushNamed(
-      //             '/list_texts',
-      //           );
-      //         } else if (result == MenuOption.Edit) {
-      //           Navigator.of(context).pushNamed(
-      //             '/update_class',
-      //           );
-      //         } else if (result == MenuOption.Remove) {
-      //           Scaffold.of(context).showSnackBar(
-      //             SnackBar(
-      //               content:
-      //                   Text('Are you sure you want to remove this class?'),
-      //             ),
-      //           );
-      //         }
-      //       },
-      //       itemBuilder: (BuildContext context) {
-      //         return <PopupMenuEntry<MenuOption>>[
-      //           PopupMenuItem(
-      //             child: Text('Assign student'),
-      //             value: MenuOption.Assign,
-      //           ),
-      //           PopupMenuItem(
-      //             child: Text('Edit class'),
-      //             value: MenuOption.Edit,
-      //           ),
-      //           PopupMenuItem(
-      //             child: Text('Remove class'),
-      //             value: MenuOption.Remove,
-      //           ),
-      //         ];
-      //       },
-      //     ),
-      //   ],
-      // ),
-      body: CustomScrollView(
-        slivers: <Widget>[
-          SliverAppBar(
-            pinned: false,
-            expandedHeight: 100.0,
-            flexibleSpace: FlexibleSpaceBar(
-              title: Hero(
-                tag: classroom.id,
-                flightShuttleBuilder: flightShuttleBuilder,
-                child: Text(
-                  'Class: Vitor Cantinho',
-                  style: TextStyle(
-                    fontSize: 22.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          SliverList(
-            delegate: SliverChildListDelegate(
-              <Widget>[
-                Hero(
-                  tag: classroom.grade,
+      body: DefaultTabController(
+        length: 2,
+        child: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                title: Hero(
+                  tag: 'name_${classroom.id}',
+                  child: Text('${classroom.name}'),
                   flightShuttleBuilder: flightShuttleBuilder,
-                  child: Text(
-                    'Grade: ${classroom.grade}',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
                 ),
-              ],
-            ),
+                pinned: true,
+                floating: true,
+                forceElevated: innerBoxIsScrolled,
+                bottom: TabBar(
+                  tabs: [
+                    Tab(text: 'STUDENTS'),
+                    Tab(text: 'TEXTS'),
+                  ],
+                ),
+              )
+            ];
+          },
+          body: TabBarView(
+            children: <Widget>[
+              StudentsPage(),
+              TextsPage(),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
