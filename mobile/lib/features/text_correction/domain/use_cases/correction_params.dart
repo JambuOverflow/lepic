68a5b3638bf685e12b1e7ba53140d781cb85cdcc
renan-cunha <renancunhafonseca@gmail.com>
2021-01-03 13:53:32 -0300

    Add CorrectionParams

---
+++ mobile/lib/features/text_correction/domain/use_cases/correction_params.dart
@@ -0,0 +1,12 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+import 'package:mobile/features/text_correction/domain/entities/correction.dart';
+
+class CorrectionParams extends Equatable {
+  final Correction correction;
+
+  CorrectionParams({@required this.correction});
+
+  @override
+  List<Object> get props => [CorrectionParams];
+}
