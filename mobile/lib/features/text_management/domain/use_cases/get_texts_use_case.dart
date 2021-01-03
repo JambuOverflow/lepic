c4da748c53ca4e430bf8e6df2da750ef191ab883
renan-cunha <renancunhafonseca@gmail.com>
2021-01-03 11:09:27 -0300

    Add get_texts_use_case.dart

--- mobile/lib/features/text_management/domain/use_cases/get_texts_use_case.dart
+++ mobile/lib/features/text_management/domain/use_cases/get_texts_use_case.dart
@@ -2,16 +2,15 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter/foundation.dart';
 import 'package:mobile/core/error/failures.dart';
 import 'package:mobile/core/use_cases/use_case.dart';
-import 'package:mobile/features/class_management/domain/use_cases/classroom_params.dart';
 import 'package:mobile/features/text_management/domain/entities/text.dart';
 import 'package:mobile/features/text_management/domain/repositories/text_repository.dart';
 
-class GetTexts implements UseCase<List<MyText>, ClassroomParams> {
+class GetTexts implements UseCase<List<MyText>, NoParams> {
   final TextRepository repository;
 
   GetTexts({@required this.repository});
 
   @override
-  Future<Either<Failure, List<MyText>>> call(ClassroomParams params) =>
-      repository.getTexts(params.classroom);
+  Future<Either<Failure, List<MyText>>> call(params) =>
+      repository.getTexts();
 }
