3cb6a8f55251ee20bbc04a58efdbcbecc801983f
renan-cunha <renancunhafonseca@gmail.com>
2021-01-03 17:26:40 -0300

    Add mistake_entity_model_converter.dart

---
+++ mobile/lib/core/data/entity_model_converters/mistake_entity_model_converter.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/rendering.dart';
+import 'package:mobile/features/text_correction/domain/entities/correction.dart';
+import 'package:mobile/features/text_correction/domain/entities/mistake.dart';
+
+import '../database.dart';
+
+class MistakeEntityModelConverter {
+  Correction mistakeModelToEntity(List<MistakeModel> models) {
+    List<Mistake> mistakes = [];
+    final int studentId = models[0].studentId;
+    final int textId = models[0].textId;
+
+    for (var model in models) {
+      var mistake = Mistake(
+        commentary: model.commentary,
+        localId: model.localId,
+        wordIndex: model.wordIndex,
+      );
+
+      if (model.studentId != studentId || model.textId != textId) {
+        throw ErrorDescription(
+          """List of mistakes contain mistakes with 
+        different studentIds or textIDs""",
+        );
+      }
+      mistakes.add(mistake);
+    }
+
+    return Correction(
+      textId: textId,
+      studentId: studentId,
+      mistakes: mistakes,
+    );
+  }
+
+  List<MistakeModel> mistakeEntityToModel(Correction entity) {
+    List<MistakeModel> mistakeModels = [];
+    MistakeModel mistakeModel;
+    for (var mistake in entity.mistakes) {
+      mistakeModel = MistakeModel(
+          studentId: entity.studentId,
+          textId: entity.textId,
+          localId: mistake.localId,
+          wordIndex: mistake.wordIndex,
+          commentary: mistake.commentary);
+      mistakeModels.add(mistakeModel);
+    }
+
+    return mistakeModels;
+  }
+}
---
+++ mobile/test/core/data/entity_model_converters/mistake_entity_model_converter_test.dart
@@ -0,0 +1,48 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mobile/core/data/database.dart';
+import 'package:mobile/core/data/entity_model_converters/mistake_entity_model_converter.dart';
+import 'package:mobile/features/text_correction/domain/entities/correction.dart';
+import 'package:mobile/features/text_correction/domain/entities/mistake.dart';
+
+void main() {
+  MistakeEntityModelConverter mistakeEntityModelConverter;
+
+  setUp(() {
+    mistakeEntityModelConverter = MistakeEntityModelConverter();
+  });
+
+  final tMistakeModel = MistakeModel(
+    wordIndex: 1,
+    commentary: "B",
+    localId: 1,
+    studentId: 2,
+    textId: 3,
+  );
+
+  final tMistakeEntity = Mistake(
+    wordIndex: 1,
+    commentary: "B",
+    localId: 1,
+  );
+
+  final tCorrection =
+      Correction(mistakes: [tMistakeEntity], studentId: 2, textId: 3);
+
+  group('modelToEntity', () {
+    test('should return a Mistake entity with proper data', () async {
+      final result =
+          mistakeEntityModelConverter.mistakeModelToEntity([tMistakeModel]);
+
+      expect(result, tCorrection);
+    });
+  });
+
+  group('entityToModel', () {
+    test('should return a Classroom model with proper data', () {
+      final result = mistakeEntityModelConverter
+          .mistakeEntityToModel(tCorrection);
+
+      expect(result, [tMistakeModel]);
+    });
+  });
+}
