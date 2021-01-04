ce1a0d9418b707f9a05b50c01e88416ed5fa48c1
renan-cunha <renancunhafonseca@gmail.com>
2021-01-04 09:50:50 -0300

    Refactor MistakeEntityModelConverter
    Extract _getMistakes method

--- mobile/lib/core/data/entity_model_converters/mistake_entity_model_converter.dart
+++ mobile/lib/core/data/entity_model_converters/mistake_entity_model_converter.dart
@@ -11,21 +11,11 @@ class MistakeEntityModelConverter {
           "'model' is empty",
         );
     }
-    List<Mistake> mistakes = [];
+    
     final int studentId = models[0].studentId;
     final int textId = models[0].textId;
 
-    for (var model in models) {
-      Mistake mistake = extractMistake(model);
-
-      if (model.studentId != studentId || model.textId != textId) {
-        throw ErrorDescription(
-          """List of mistakes contain mistakes with 
-        different studentIds or textIDs""",
-        );
-      }
-      mistakes.add(mistake);
-    }
+    List<Mistake> mistakes = _getMistakes(models, studentId, textId);
 
     return Correction(
       textId: textId,
@@ -34,7 +24,23 @@ class MistakeEntityModelConverter {
     );
   }
 
-  Mistake extractMistake(MistakeModel model) {
+  List<Mistake> _getMistakes(List<MistakeModel> models, int studentId, int textId) {
+    List<Mistake> mistakes = [];
+    for (var model in models) {
+      Mistake mistake = _extractMistake(model);
+    
+      if (model.studentId != studentId || model.textId != textId) {
+        throw ErrorDescription(
+          """List of mistakes contain mistakes with 
+        different studentIds or textIDs""",
+        );
+      }
+      mistakes.add(mistake);
+    }
+    return mistakes;
+  }
+
+  Mistake _extractMistake(MistakeModel model) {
     var mistake = Mistake(
       commentary: model.commentary,
       localId: model.localId,
