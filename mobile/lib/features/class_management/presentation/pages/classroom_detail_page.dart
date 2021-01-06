b24b58dafe24343b7b43e5856d96049b8484bdc7
VitorCantao <vitornovaes.cantao@gmail.com>
2021-01-05 21:11:31 -0300

    Instantiate bloc with classroom when building students page

--- mobile/lib/features/class_management/presentation/pages/classroom_detail_page.dart
+++ mobile/lib/features/class_management/presentation/pages/classroom_detail_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mobile/features/student_management/presentation/bloc/student_bloc.dart';
 
 import '../../domain/entities/classroom.dart';
 import '../../../student_management/presentation/pages/students_page.dart';
@@ -40,7 +43,11 @@ class ClassroomDetailPage extends StatelessWidget {
           },
           body: TabBarView(
             children: <Widget>[
-              StudentsPage(),
+              BlocProvider(
+                create: (context) =>
+                    GetIt.instance<StudentBloc>(param1: classroom),
+                child: StudentsPage(),
+              ),
               TextsPage(),
             ],
           ),
