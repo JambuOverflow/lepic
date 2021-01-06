7dd63e4b127cc22a9039af567d91082ece9c1bc1
João Canavarro <jvcanavarro@gmail.com>
2021-01-06 15:19:18 -0300

    Update student item with gesture detector

--- mobile/lib/features/student_management/presentation/widgets/student_item.dart
+++ mobile/lib/features/student_management/presentation/widgets/student_item.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mobile/features/student_management/presentation/bloc/student_bloc.dart';
+import 'package:mobile/features/student_management/presentation/pages/create_student_page.dart';
 
 import '../../domain/entities/student.dart';
 import '../pages/student_detail_page.dart';
@@ -10,21 +13,32 @@ class StudentItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        title: Text(_student.firstName.toString()),
-        subtitle: Text(_student.classroomId.toString()),
-        trailing: IconButton(
-          icon: Icon(Icons.arrow_forward),
-          onPressed: () {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => StudentDetailPage(_student),
-              ),
-            );
-          },
+    return GestureDetector(
+        child: Card(
+          child: ListTile(
+            title: Text(_student.firstName.toString()),
+            subtitle: Text(_student.classroomId.toString()),
+            trailing: IconButton(
+              icon: Icon(Icons.arrow_forward),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => StudentDetailPage(_student),
+                  ),
+                );
+              },
+            ),
+          ),
         ),
-      ),
-    );
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute<CreateStudentPage>(
+              builder: (_) => BlocProvider.value(
+                value: BlocProvider.of<StudentBloc>(context),
+                child: CreateStudentPage(),
+              ),
+            ),
+          );
+        });
   }
 }
