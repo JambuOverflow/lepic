be70862e88fd79ff1bcd019ebabffe677292c5a8
FelipOliveira <sem.ideias.na.hora@gmail.com>
2021-01-13 10:42:00 -0300

    add-MyText-file-and-refactor

--- mobile/lib/features/audio_management/presentation/pages/audio_upload.dart
+++ mobile/lib/features/audio_management/presentation/pages/audio_upload.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/features/text_management/domain/entities/text.dart';
 
 class AudioPage extends StatefulWidget {
   AudioPage({Key key}) : super(key: key);
@@ -9,6 +10,12 @@ class AudioPage extends StatefulWidget {
 
 class _AudioPageState extends State<AudioPage> {
   bool recording = false;
+  final MyText myText = new MyText(
+    title: 'title',
+    body:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
+    classId: 01,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +37,9 @@ class _AudioPageState extends State<AudioPage> {
                   color: Colors.white,
                   size: 28,
                 ),
-                onPressed: () {},
+                onPressed: () {
+                  // restart the recording
+                },
               ),
               Expanded(
                 child: Text(
@@ -45,7 +54,9 @@ class _AudioPageState extends State<AudioPage> {
                       size: 28,
                       color: Colors.white,
                     ),
-                    onPressed: () {},
+                    onPressed: () {
+                      // Upload the audio file
+                    },
                   ),
                   Padding(
                     padding: EdgeInsets.only(right: 5),
@@ -58,31 +69,27 @@ class _AudioPageState extends State<AudioPage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
+          // Begin/stop the audio recording
           setState(() {
             recording = !recording;
           });
-          print(recording);
+          print("recording: $recording");
         },
         child: recording ? Icon(Icons.stop) : Icon(Icons.mic),
         tooltip: 'Upload Audio',
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       body: SingleChildScrollView(
-        padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
+        padding: EdgeInsets.symmetric(vertical: 32.0, horizontal: 24.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
+          // crossAxisAlignment: CrossAxisAlignment.start,
+          // mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             Text(
-              'Text title',
+              myText.title,
               style: TextStyle(fontSize: 24.0),
             ),
-            Text('We move under cover and we move as one'),
-            Text('Through the night, we have one shot to live another day'),
-            Text('We cannot let a stray gunshot give us away'),
-            Text('We will fight up close, seize the moment and stay in it'),
-            Text('It’s either that or meet the business end of a bayonet'),
-            Text('The code word is ‘Rochambeau,’ dig me?'),
+            Text(myText.body),
           ],
         ),
       ),
