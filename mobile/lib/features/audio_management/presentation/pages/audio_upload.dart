6c6de6cc7bcd3b413dc26224e6ac7c0de708c205
FelipOliveira <sem.ideias.na.hora@gmail.com>
2021-01-14 10:25:14 -0300

    add-timer-to-audio-recording

--- mobile/lib/features/audio_management/presentation/pages/audio_upload.dart
+++ mobile/lib/features/audio_management/presentation/pages/audio_upload.dart
@@ -1,4 +1,9 @@
+// import 'dart:async';
+
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:mobile/features/audio_management/presentation/widgets/stopwatch.dart';
 import 'package:mobile/features/text_management/domain/entities/text.dart';
 
 class AudioPage extends StatefulWidget {
@@ -10,59 +15,66 @@ class AudioPage extends StatefulWidget {
 
 class _AudioPageState extends State<AudioPage> {
   bool recording = false;
-  final MyText myText = new MyText(
-    title: 'title',
-    body:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
-    classId: 01,
-  );
+
+  StreamSubscription<int> timerSubscription;
+  String minutesStr = '00';
+  String secondsStr = '00';
+
+  final MyText myTextTest = new MyText(
+      title: 'title',
+      body:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
+      classId: 2);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Record audio')),
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.replay),
+            tooltip: 'replay recording',
+            onPressed: () {
+              print('replay recording');
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.delete),
+            tooltip: 'delete recording',
+            onPressed: () {
+              print('record deleted');
+            },
+          ),
+        ],
+        title: Text('Record audio'),
+      ),
       bottomNavigationBar: BottomAppBar(
-        color: Colors.grey,
+        color: Colors.grey[700],
         child: Container(
-          decoration: BoxDecoration(
-              border: Border(
-            top: BorderSide(color: Colors.grey, width: 0.3),
-          )),
           child: Row(
             mainAxisSize: MainAxisSize.max,
             children: <Widget>[
-              IconButton(
-                icon: Icon(
-                  Icons.replay,
-                  color: Colors.white,
-                  size: 28,
-                ),
-                onPressed: () {
-                  // restart the recording
-                },
+              Padding(
+                padding: EdgeInsets.only(right: 5, left: 5),
               ),
               Expanded(
-                child: Text(
-                  "",
-                ),
-              ),
-              Wrap(
-                children: <Widget>[
-                  IconButton(
-                    icon: Icon(
-                      Icons.send,
-                      size: 28,
+                child: Text("$minutesStr:$secondsStr",
+                    style: TextStyle(
                       color: Colors.white,
-                    ),
-                    onPressed: () {
-                      // Upload the audio file
-                    },
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(right: 5),
-                  )
-                ],
-              )
+                    )),
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.send,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  // Upload the audio file
+                  recording
+                      ? print('stop the recording first!')
+                      : print('audio sended - $minutesStr:$secondsStr');
+                },
+              ),
             ],
           ),
         ),
@@ -72,27 +84,43 @@ class _AudioPageState extends State<AudioPage> {
           // Begin/stop the audio recording
           setState(() {
             recording = !recording;
+            stopWatch();
           });
-          print("recording: $recording");
         },
         child: recording ? Icon(Icons.stop) : Icon(Icons.mic),
-        tooltip: 'Upload Audio',
+        tooltip: 'Record/Stop Audio',
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       body: SingleChildScrollView(
-        padding: EdgeInsets.symmetric(vertical: 32.0, horizontal: 24.0),
+        padding: EdgeInsets.fromLTRB(24.0, 24.0, 24.0, 32.0),
         child: Column(
-          // crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           // mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             Text(
-              myText.title,
+              myTextTest.title,
               style: TextStyle(fontSize: 24.0),
             ),
-            Text(myText.body),
+            Text(myTextTest.body),
           ],
         ),
       ),
     );
+  }
+
+  void stopWatch() {
+    print("recording: $recording");
+    if (recording) {
+      Stream<int> timerStream = stopWatchStream();
+      timerSubscription = timerStream.listen((int newTick) {
+        setState(() {
+          minutesStr = ((newTick / 60) % 60).floor().toString().padLeft(2, '0');
+          secondsStr = (newTick % 60).floor().toString().padLeft(2, '0');
+        });
+      });
+    } else {
+      timerSubscription.cancel();
+      // timerStream = null;
+    }
   }
 }
---
+++ mobile/lib/features/audio_management/presentation/widgets/stopwatch.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+Stream<int> stopWatchStream() {
+  StreamController<int> streamController;
+  Timer timer;
+  Duration timerInterval = Duration(seconds: 1);
+  int counter = 0;
+
+  void stopTimer() {
+    if (timer != null) {
+      timer.cancel();
+      timer = null;
+      counter = 0;
+      streamController.close();
+    }
+  }
+
+  void tick(_) {
+    counter++;
+    streamController.add(counter);
+  }
+
+  void startTimer() {
+    timer = Timer.periodic(timerInterval, tick);
+  }
+
+  streamController = StreamController<int>(
+    onListen: startTimer,
+    onCancel: stopTimer,
+    onResume: startTimer,
+    onPause: stopTimer,
+  );
+
+  return streamController.stream;
+}
