9c186f1850de95ee3b9e332da86fb82b052444c8
FelipOliveira <sem.ideias.na.hora@gmail.com>
2021-01-14 16:13:18 -0300

    generated-pagerouter-to audio-page

--- mobile/lib/features/audio_management/presentation/pages/audio_upload.dart
+++ mobile/lib/features/audio_management/presentation/pages/audio_upload.dart
@@ -7,8 +7,8 @@ import 'package:mobile/features/audio_management/presentation/widgets/stopwatch.
 import 'package:mobile/features/text_management/domain/entities/text.dart';
 
 class AudioPage extends StatefulWidget {
-  AudioPage({Key key}) : super(key: key);
-
+  final MyText myTextTest;
+  AudioPage({this.myTextTest, Key key}) : super(key: key);
   @override
   _AudioPageState createState() => _AudioPageState();
 }
@@ -19,12 +19,6 @@ class _AudioPageState extends State<AudioPage> {
   StreamSubscription<int> timerSubscription;
   String minutesStr = '00';
   String secondsStr = '00';
-
-  final MyText myTextTest = new MyText(
-      title: 'title',
-      body:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
-      classId: 2);
 
   @override
   Widget build(BuildContext context) {
@@ -98,10 +92,10 @@ class _AudioPageState extends State<AudioPage> {
           // mainAxisSize: MainAxisSize.min,
           children: <Widget>[
             Text(
-              myTextTest.title,
+              widget.myTextTest.title,
               style: TextStyle(fontSize: 24.0),
             ),
-            Text(myTextTest.body),
+            Text(widget.myTextTest.body),
           ],
         ),
       ),
