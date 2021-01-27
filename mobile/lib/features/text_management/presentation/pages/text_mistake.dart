714aceb2cae0795abf69d3531f010f5b17e8f15e
VitorCantao <vitornovaes.cantao@gmail.com>
2021-01-27 13:26:58 -0300

    Update text mistake

--- mobile/lib/features/text_management/presentation/pages/text_mistake.dart
+++ mobile/lib/features/text_management/presentation/pages/text_mistake.dart
@@ -1,145 +1,112 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../utils/word_section.dart';
-import '../widgets/comments_bottom_sheet.dart';
+import '../../../text_correction/presentation/widgets/correction_positioned_overlay.dart';
+import '../../../text_correction/presentation/widgets/highlightable_text.dart';
+import '../../../text_correction/presentation/bloc/correction_bloc.dart';
 
 class TextMistake extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    return _TextMistakeState();
-  }
+  _TextMistakeState createState() => _TextMistakeState();
 }
 
 class _TextMistakeState extends State<TextMistake> {
-  List<WordSection> _sections;
-
-  String testText =
-      '''Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sagittis nulla augue, eget tincidunt massa lobortis ut. Vivamus justo eros, maximus non rutrum et, venenatis id lacus. Suspendisse potenti. Vivamus sit amet dolor nisl. Etiam et nisl ut nibh eleifend suscipit a ac neque. Sed imperdiet orci sed porttitor dignissim.''';
-
-  @override
-  void initState() {
-    _sections = List<WordSection>();
-    super.initState();
-  }
+  OverlayEntry entry;
+  CorrectionBloc bloc;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: _buildTextSpanWithSplittedText(testText, context),
-      ),
+    bloc = BlocProvider.of<CorrectionBloc>(context);
+
+    return BlocBuilder<CorrectionBloc, CorrectionState>(
+      builder: (_, state) {
+        return Container(
+          alignment: Alignment.center,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: _buildTextSpanWithSplittedText(context),
+          ),
+        );
+      },
     );
   }
 
-  RichText _buildTextSpanWithSplittedText(
-    String textToSplit,
-    BuildContext context,
-  ) {
-    final splittedText = textToSplit.split(" ");
+  RichText _buildTextSpanWithSplittedText(BuildContext context) {
+    final text = bloc.text.body;
+
+    final splittedText = text.split(" ");
     final spans = List<TextSpan>();
 
     for (int i = 0; i <= splittedText.length - 1; i++) {
-      var tapSection = WordSection(
-        commentFunction: () {
-          _showTextCommentBottomSheet(context, splittedText[i], _sections[i]);
-          setState(() {});
-        },
-        highlightFunction: () {
-          print('Text is: ${splittedText[i]} ($i)');
-          setState(() {});
-        },
-      );
-
-      _sections.add(tapSection);
-      _addClickableWords(
-        spans: spans,
-        word: splittedText[i],
-        section: _sections[i],
-      );
-      _addNonClickableSpaces(spans);
+      spans.add(_buildClickableWord(index: i));
+      spans.add(_nonClickableSpace());
     }
 
     return RichText(
       text: TextSpan(
         children: spans,
-        style: TextStyle(fontSize: 22, height: 1.25),
+        style: TextStyle(fontSize: 24, height: 1.25),
       ),
     );
   }
 
-  void _showTextCommentBottomSheet(
-    BuildContext context,
-    String word,
-    WordSection section,
-  ) {
-    showModalBottomSheet(
-      context: context,
-      isScrollControlled: true,
-      barrierColor: Colors.black.withOpacity(0.2),
-      builder: (BuildContext context) => CommentsBottomSheet(
-        wordToComment: word,
-        section: section,
-      ),
-    );
-  }
+  TextSpan _nonClickableSpace() =>
+      TextSpan(text: ' ', style: TextStyle(fontSize: 22));
 
-  void _addClickableWords({
-    List<TextSpan> spans,
-    String word,
-    WordSection section,
-  }) {
-    spans.add(
-      TextSpan(
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 22,
-        ),
-        children: <InlineSpan>[
-          WidgetSpan(
-            child: GestureDetector(
-              onTap: () {
-                if (section.hasComment) return section.commentFunction();
+  TextSpan _buildClickableWord({@required int index}) {
+    GlobalKey key = GlobalKey();
 
-                section.isPressed = !section.isPressed;
-                return section.highlightFunction();
-              },
-              onLongPress: () {
-                section.hasComment = true;
-                return section.commentFunction();
-              },
-              child: Text(
-                word,
-                style: TextStyle(
-                  backgroundColor: _highlightText(section),
-                  decoration:
-                      section.hasComment ? TextDecoration.underline : null,
-                  decorationThickness: 1.3,
-                  fontSize: 22,
-                ),
-              ),
-            ),
+    return TextSpan(
+      style: TextStyle(color: Colors.black, fontSize: 22),
+      children: <InlineSpan>[
+        WidgetSpan(
+          child: HighlightableText(
+            index: index,
+            key: key,
+            onTap: () {
+              if (bloc.indexToMistakes[index] == null)
+                bloc.add(HighlightEvent(wordIndex: index));
+
+              _buildCorrectionOverlay(
+                wordIndex: index,
+                context: context,
+                renderBox: key.currentContext.findRenderObject(),
+              );
+            },
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
-  Color _highlightText(WordSection section) {
-    if (section.hasComment)
-      return Colors.orange[600];
-    else if (section.isPressed)
-      return Colors.amber;
-    else
-      return null;
-  }
+  _buildCorrectionOverlay({
+    int wordIndex,
+    BuildContext context,
+    RenderBox renderBox,
+  }) {
+    entry = OverlayEntry(
+      builder: (_) {
+        final offset = renderBox.localToGlobal(
+          Offset(renderBox.size.width * 0.25, 0),
+        );
 
-  void _addNonClickableSpaces(List<TextSpan> spans) {
-    spans.add(TextSpan(
-      text: ' ',
-      style: TextStyle(fontSize: 22),
-    ));
+        final deviceSize = MediaQuery.of(context).size;
+        final horizontalLimit =
+            MediaQuery.of(context).orientation == Orientation.portrait
+                ? deviceSize.width / 2
+                : deviceSize.height * 1.6;
+
+        return CorrectionPositionedOverlay(
+          wordIndex: wordIndex,
+          entry: entry,
+          offset: offset,
+          deviceSize: deviceSize,
+          horizontalLimit: horizontalLimit,
+          parentContext: context,
+        );
+      },
+    );
+
+    Overlay.of(context).insert(entry);
   }
 }
-
