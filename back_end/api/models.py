9ed810a0f25f68ea3ddd761fe1aa09640fdcdc6f
Arouck <ppvitorr@gmail.com>
2021-01-28 16:27:51 -0300

    Remove 2nd local_id attr from all classes

--- back_end/api/models.py
+++ back_end/api/models.py
@@ -17,7 +17,6 @@ class User(AbstractUser):
 
 
 class School(models.Model):
-    local_id = models.PositiveIntegerField()
     name = models.CharField(max_length=100)
     city = models.CharField(max_length=30)
     neighbourhood = models.CharField(max_length=30)
@@ -40,7 +39,6 @@ class School(models.Model):
 
 
 class Class(models.Model):
-    local_id = models.PositiveIntegerField()
     tutor = models.ForeignKey(User, on_delete=models.CASCADE)
     school = models.ForeignKey(School, on_delete=models.CASCADE)
     grade = models.IntegerField()
@@ -54,7 +52,6 @@ class Class(models.Model):
 
 
 class Text(models.Model):
-    local_id = models.PositiveIntegerField()
     title = models.CharField(max_length=50)
     body = models.CharField(max_length=1000)
     _class = models.ForeignKey(Class, on_delete=models.CASCADE)
@@ -67,7 +64,6 @@ class Text(models.Model):
 
 
 class Student(models.Model):
-    local_id = models.PositiveIntegerField()
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     _class = models.ForeignKey(Class, on_delete=models.CASCADE)
@@ -80,7 +76,6 @@ class Student(models.Model):
 
 
 class AudioFile(models.Model):
-    local_id = models.PositiveIntegerField()
     title = models.CharField(max_length=200)
     text = models.ForeignKey(Text, on_delete=models.CASCADE)
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
