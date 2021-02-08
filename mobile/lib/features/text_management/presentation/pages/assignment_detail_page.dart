23f8b0269045e0d89dab2e9f4b0f886023302ac1
VitorCantao <vitornovaes.cantao@gmail.com>
2021-02-08 14:40:19 -0300

    Add assignment detail page

---
+++ mobile/lib/features/text_management/presentation/pages/assignment_detail_page.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+
+import '../../../text_correction/domain/use_cases/get_correction_use_case.dart';
+import '../../../text_correction/presentation/bloc/correction_bloc.dart';
+import '../widgets/assignment_contextual_floating_action_button.dart';
+import '../widgets/audio_preview_card.dart';
+import '../widgets/correction_preview_cards.dart';
+import '../widgets/text_preview_card.dart';
+import '../widgets/app_bar_bottom_text_title.dart';
+import '../../../student_management/domain/entities/student.dart';
+import '../../domain/entities/text.dart';
+import '../bloc/text_bloc.dart';
+
+class AssigmentDetailPage extends StatefulWidget {
+  final int textIndex;
+
+  const AssigmentDetailPage({
+    Key key,
+    @required this.textIndex,
+  }) : super(key: key);
+
+  @override
+  _AssigmentDetailPageState createState() => _AssigmentDetailPageState();
+}
+
+class _AssigmentDetailPageState extends State<AssigmentDetailPage> {
+  final _scrollController = ScrollController();
+
+  TextBloc textBloc;
+  CorrectionBloc correctionBloc;
+
+  Student student;
+
+  @override
+  void initState() {
+    textBloc = BlocProvider.of<TextBloc>(context);
+    student = textBloc.student;
+    super.initState();
+  }
+
+  MyText get _text => textBloc.texts[widget.textIndex];
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => correctionBloc = GetIt.instance.get<CorrectionBloc>(
+            param1: StudentTextParams(text: _text, student: student),
+          ),
+        ),
+      ],
+      child: BlocBuilder<TextBloc, TextState>(
+        builder: (context, state) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('${student.firstName} ${student.lastName}'),
+              bottom: AppBarBottomTextTitle(title: _text.title),
+            ),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+            floatingActionButton: AssignmentContextualFloatingActionButton(),
+            body: Scrollbar(
+              controller: _scrollController,
+              isAlwaysShown: true,
+              child: ListView(
+                padding: EdgeInsets.all(16),
+                children: [
+                  TextPreviewCard(text: _text),
+                  SizedBox(height: 8),
+                  AudioPreviewCard(),
+                  SizedBox(height: 8),
+                  CorrectionPreviewCard(
+                    params: StudentTextParams(student: student, text: _text),
+                  ),
+                  SizedBox(height: 8),
+                  Card(child: Container(height: 200)),
+                  SizedBox(height: 64),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
