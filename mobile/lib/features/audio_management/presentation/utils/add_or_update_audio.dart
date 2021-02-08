e1bd4890a593638e16c9a2b07626defa2df84c1d
João Canavarro <jvcanavarro@gmail.com>
2021-02-08 12:42:29 -0300

    Remove optional arg from add/update audio method

--- mobile/lib/features/audio_management/presentation/utils/add_or_update_audio.dart
+++ mobile/lib/features/audio_management/presentation/utils/add_or_update_audio.dart
@@ -4,13 +4,12 @@ import 'dart:typed_data';
 import 'package:file_picker/file_picker.dart';
 import 'package:mobile/features/audio_management/presentation/bloc/audio_bloc.dart';
 
-void addOrUpdateAudio(bloc, [title]) async {
+void addOrUpdateAudio(bloc) async {
   PlatformFile file =
       (await FilePicker.platform.pickFiles(type: FileType.audio))?.files[0];
   Uint8List audioBytes = File(file.path).readAsBytesSync();
   bloc.isAudioAttached
       ? bloc.add(UpdateAudioEvent(
           oldAudio: bloc.audio, audioData: audioBytes, title: file.name))
-      : bloc.add(CreateAudioEvent(
-          audioData: audioBytes, title: title ? title : file.name));
+      : bloc.add(CreateAudioEvent(audioData: audioBytes, title: file.name));
 }
