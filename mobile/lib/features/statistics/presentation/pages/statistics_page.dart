62d5a97aebf6ff7eedc3d5da2cd5f39180527b31
João Canavarro <jvcanavarro@gmail.com>
2021-02-17 20:00:13 -0300

    Add statistics page

---
+++ mobile/lib/features/statistics/presentation/pages/statistics_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/core/presentation/widgets/background_app_bar.dart';
+
+class StatisticsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: BackgroundAppBar(title: 'Statistics'),
+    );
+  }
+}
