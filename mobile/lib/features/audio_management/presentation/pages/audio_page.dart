d40bedf748aef5ab56eea1bdf36162f9f2dc249b
VitorCantao <vitornovaes.cantao@gmail.com>
2021-02-19 14:07:59 -0300

    Transform audio page to fit bottom sheet

--- mobile/lib/features/audio_management/presentation/pages/audio_page.dart
+++ mobile/lib/features/audio_management/presentation/pages/audio_page.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'package:mobile/core/presentation/widgets/background_app_bar.dart';
-import 'package:mobile/features/audio_management/presentation/bloc/audio_bloc.dart';
-import 'package:mobile/features/audio_management/presentation/widgets/appbar_icon_builder.dart';
-import 'package:mobile/features/audio_management/presentation/widgets/audio_item.dart';
+import '../bloc/audio_bloc.dart';
+import '../widgets/audio_item.dart';
 
 class AudioPage extends StatefulWidget {
-  AudioPage({Key key}) : super(key: key);
   @override
   _AudioPageState createState() => _AudioPageState();
 }
@@ -24,49 +21,28 @@ class _AudioPageState extends State<AudioPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: BackgroundAppBar(
-        title: 'Editing Audio',
-        actions: <Widget>[
-          BlocBuilder<AudioBloc, AudioState>(
+    return SingleChildScrollView(
+      padding: EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          BlocConsumer<AudioBloc, AudioState>(
             builder: (context, state) {
-              return AppBarIconBuilder(
-                bloc: _bloc,
-              );
+              if (state is AudioLoadInProgress)
+                return CircularProgressIndicator();
+              else if (_bloc.audio == null)
+                return ListTile(title: Text('No audio uploaded yet.'));
+              else
+                return AudioItem(bloc: _bloc);
+            },
+            listener: (context, state) {
+              if (state is Error && _bloc.isAudioAttached)
+                Scaffold.of(context).showSnackBar(
+                  SnackBar(content: Text(state.message)),
+                );
             },
           ),
         ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.done),
-        onPressed: () => Navigator.pop(context),
-      ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(22.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            BlocConsumer<AudioBloc, AudioState>(
-              builder: (context, state) {
-                if (state is AudioLoadInProgress)
-                  return CircularProgressIndicator();
-                else if (_bloc.audio == null)
-                  return ListTile(title: Text('No audio uploaded yet.'));
-                else
-                  return AudioItem(bloc: _bloc);
-              },
-              listener: (context, state) {
-                if (state is Error && _bloc.isAudioAttached)
-                  Scaffold.of(context).showSnackBar(
-                    SnackBar(content: Text(state.message)),
-                  );
-              },
-            ),
-            SizedBox(height: 20),
-            Text(_bloc.text.title, style: TextStyle(fontSize: 24.0)),
-            Text(_bloc.text.body),
-          ],
-        ),
       ),
     );
   }
