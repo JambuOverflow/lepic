bf326244a154c888dd503e3c4a6e21d6f04d4c51
João Canavarro <jvcanavarro@gmail.com>
2021-02-19 20:02:58 -0300

    Populate statistics page

--- mobile/lib/features/statistics/presentation/pages/statistics_page.dart
+++ mobile/lib/features/statistics/presentation/pages/statistics_page.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:mobile/core/presentation/widgets/background_app_bar.dart';
+import 'package:mobile/features/student_management/domain/entities/student.dart';
 
 class StatisticsPage extends StatelessWidget {
+  final Student _student;
+  final String _content;
+
+  const StatisticsPage(
+      {Key key, @required Student student, @required String content})
+      : _student = student,
+        _content = content,
+        super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: BackgroundAppBar(title: 'Statistics'),
+      body: Scrollbar(
+        child: Hero(
+          tag: '${_student.id}_body',
+          child: Text(_content),
+        ),
+      ),
     );
   }
 }
