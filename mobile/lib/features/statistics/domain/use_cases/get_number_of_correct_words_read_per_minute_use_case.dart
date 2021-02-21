e714e0bcbcc7d31b0ce2f15e3f37628de280c154
renan-cunha <renancunhafonseca@gmail.com>
2021-02-21 19:57:12 -0300

    Modify GetNumberOfCorrectWordsPerMinute use case. Change its input to Correction

--- mobile/lib/features/statistics/domain/use_cases/get_number_of_correct_words_read_per_minute_use_case.dart
+++ mobile/lib/features/statistics/domain/use_cases/get_number_of_correct_words_read_per_minute_use_case.dart
@@ -3,28 +3,28 @@ import 'package:flutter/foundation.dart';
 import 'package:mobile/core/error/exceptions.dart';
 import 'package:mobile/core/error/failures.dart';
 import 'package:mobile/core/use_cases/use_case.dart';
-import 'package:mobile/features/audio_management/domain/use_cases/audio_params.dart';
-import 'package:mobile/features/text_correction/domain/entities/correction.dart';
+import 'package:mobile/features/audio_management/domain/use_cases/get_audio_from_id_use_case.dart';
+import 'package:mobile/features/text_correction/domain/use_cases/correction_params.dart';
 import 'package:mobile/features/text_correction/domain/use_cases/get_correction_from_id_use_case.dart';
-import 'package:mobile/features/text_management/domain/entities/text.dart';
 import 'package:mobile/features/text_management/domain/use_cases/get_text_use_case.dart';
 
 class GetNumberOfCorrectWordsReadPerMinuteUseCase
-    implements UseCase<double, AudioParams> {
+    implements UseCase<double, CorrectionParams> {
   final GetTextUseCase getTextUseCase;
-  final GetCorrectionFromIdUseCase getCorrectionFromIdUseCase;
+  final GetAudioFromIdUseCase getAudioFromIdUseCase;
 
   GetNumberOfCorrectWordsReadPerMinuteUseCase({
     @required this.getTextUseCase,
-    @required this.getCorrectionFromIdUseCase,
+    @required this.getAudioFromIdUseCase,
   });
 
   @override
-  Future<Either<Failure, double>> call(AudioParams params) async {
+  Future<Either<Failure, double>> call(CorrectionParams params) async {
     try {
       final int numberOfwords = await _getNumberOfWords(params);
-      final int numberMistakes = await _getNumberOfMistakes(params);
-      final Duration audioDuration = params.audio.audioDuration;
+      final int numberMistakes = params.correction.numberOfMistakes;
+
+      final Duration audioDuration = await _getAudioDuration(params);
 
       return Right((numberOfwords - numberMistakes) / audioDuration.inMinutes);
     } on CacheException catch (e) {
@@ -32,25 +32,23 @@ class GetNumberOfCorrectWordsReadPerMinuteUseCase
     }
   }
 
-  Future<int> _getNumberOfMistakes(AudioParams params) async {
-    final int studentId = params.audio.studentId;
-      final int textId = params.audio.textId;
+  Future<Duration> _getAudioDuration(CorrectionParams params) async {
+    final int studentId = params.correction.studentId;
+    final int textId = params.correction.textId;
     final CorrectionIdParams correctionIdParams = CorrectionIdParams(
       textId: textId,
       studentId: studentId,
     );
-    final correctionOutput =
-        await getCorrectionFromIdUseCase(correctionIdParams);
-
-    return correctionOutput.fold((l) {
+    final audioOutput = await getAudioFromIdUseCase(correctionIdParams);
+    return audioOutput.fold((l) {
       throw CacheException();
     }, (r) {
-      return r.numberOfMistakes;
+      return r.audioDuration;
     });
   }
 
-  Future<int> _getNumberOfWords(AudioParams params) async {
-    final textEither = await getTextUseCase(params.audio.textId);
+  Future<int> _getNumberOfWords(CorrectionParams params) async {
+    final textEither = await getTextUseCase(params.correction.textId);
     return textEither.fold((l) {
       throw CacheException();
     }, (r) {
