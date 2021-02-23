53cce53846eb98ab77853b55c64eed16e836c035
renan-cunha <renancunhafonseca@gmail.com>
2021-02-23 19:24:22 -0300

    Modify MistakeLocalDataSource with CorrectionModel

--- mobile/lib/features/text_correction/data/data_sources/mistake_local_data_source.dart
+++ mobile/lib/features/text_correction/data/data_sources/mistake_local_data_source.dart
@@ -9,7 +9,13 @@ abstract class MistakeLocalDataSource {
   /// Returns an empty list if no [Mistake] is cache.
   ///
   /// Throws [CacheException] if something wrong happens.
-  Future<List<MistakeModel>> getCorrectionMistakesFromCache({
+  Future<List<MistakeModel>> getMistakesFromCache(
+      CorrectionModel correctionModel);
+
+  /// Gets the cached of [Correction]
+  ///
+  /// Throws [CacheException] if something wrong happens.
+  Future<CorrectionModel> getCorrectionFromCache({
     StudentModel studentModel,
     TextModel textModel,
   });
@@ -19,21 +25,30 @@ abstract class MistakeLocalDataSource {
   /// Returns an empty list if no [Mistake] is cache.
   ///
   /// Throws [CacheException] if something wrong happens.
-  Future<List<MistakeModel>> getCorrectionMistakesFromCacheUsingId({
+  Future<List<MistakeModel>> getMistakesFromCacheUsingId(int correctionId);
+
+  /// Gets  [Correction]
+  ///
+  /// Throws [CacheException] if something wrong happens.
+  Future<CorrectionModel> getCorrectionFromCacheUsingId({
     int studentId,
     int textId,
   });
 
-  /// Deletes all the [Mistake] from a specific collection.
+  /// Deletes the [Correction]
   ///
-  /// Throws [CacheException] if the [Collection] is not cached.
-  Future<void> deleteCorrectionMistakesFromCache(
-      MistakeModel mistakeModel);
+  /// Throws [CacheException] if the [Correction] is not cached.
+  Future<void> deleteCorrectionFromCache(CorrectionModel correctionModel);
 
   /// Caches the new [Mistake] passed.
   ///
   /// Throws [CacheException] if something wrong happens.
   Future<MistakeModel> cacheNewMistake(MistakeModel mistakeModel);
+
+  /// Caches the new [Correction] passed.
+  ///
+  /// Throws [CacheException] if something wrong happens.
+  Future<CorrectionModel> cacheNewCorrection(CorrectionModel correctionModel);
 }
 
 class MistakeLocalDataSourceImpl implements MistakeLocalDataSource {
@@ -54,44 +69,76 @@ class MistakeLocalDataSourceImpl implements MistakeLocalDataSource {
   }
 
   @override
-  Future<void> deleteCorrectionMistakesFromCache(
-      MistakeModel mistakeModel) async {
-    final int studentPk = mistakeModel.studentId;
-    final int textPk = mistakeModel.textId;
+  Future<void> deleteCorrectionFromCache(
+      CorrectionModel correctionModel) async {
+    final int correctionPk = correctionModel.localId;
     try {
-      await this.database.deleteMistakesOfCorrection(
-            textPk: textPk,
-            studentPk: studentPk,
-          );
+      await this.database.deleteMistakesOfCorrection(correctionPk);
+      await this.database.deleteCorrection(correctionPk);
     } on SqliteException {
       throw CacheException();
     }
   }
 
   @override
-  Future<List<MistakeModel>> getCorrectionMistakesFromCache(
+  Future<List<MistakeModel>> getMistakesFromCache(
+      CorrectionModel correctionModel) async {
+    final int correctionId = correctionModel.localId;
+    try {
+      return await this.database.getMistakesOfCorrection(correctionId);
+    } on SqliteException {
+      throw CacheException();
+    }
+  }
+
+  @override
+  Future<List<MistakeModel>> getMistakesFromCacheUsingId(
+      int correctionId) async {
+    try {
+      return await this.database.getMistakesOfCorrection(correctionId);
+    } on SqliteException {
+      throw CacheException();
+    }
+  }
+
+  @override
+  Future<CorrectionModel> cacheNewCorrection(
+      CorrectionModel correctionModel) async {
+    try {
+      final pk = await this.database.insertCorrection(correctionModel);
+      return correctionModel.copyWith(localId: pk);
+    } on SqliteException {
+      throw CacheException();
+    }
+  }
+
+  @override
+  Future<CorrectionModel> getCorrectionFromCache(
       {StudentModel studentModel, TextModel textModel}) async {
     final int studentId = studentModel.localId;
     final int textId = textModel.localId;
     try {
-      return await this.database.getMistakesOfCorrection(
-            textPk: textId,
-            studentPk: studentId,
-          );
+      return await this
+          .database
+          .getCorrection(studentId: studentId, textId: textId);
     } on SqliteException {
       throw CacheException();
+    } on EmptyDataException {
+      throw EmptyDataException();
     }
   }
 
   @override
-  Future<List<MistakeModel>> getCorrectionMistakesFromCacheUsingId({int studentId, int textId}) async {
+  Future<CorrectionModel> getCorrectionFromCacheUsingId(
+      {int studentId, int textId}) async {
     try {
-      return await this.database.getMistakesOfCorrection(
-            textPk: textId,
-            studentPk: studentId,
-          );
+      return await this
+          .database
+          .getCorrection(studentId: studentId, textId: textId);
     } on SqliteException {
       throw CacheException();
+    } on EmptyDataException {
+      throw EmptyDataException();
     }
   }
 }
