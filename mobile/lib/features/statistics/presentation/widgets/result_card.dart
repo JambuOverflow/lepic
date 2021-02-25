2618d6291dafefffa690c17622b671d1d2b00cca
João Canavarro <jvcanavarro@gmail.com>
2021-02-25 15:19:56 -0300

    Rename info_card to result_card

---
+++ mobile/lib/features/statistics/presentation/widgets/result_card.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:mobile/features/statistics/presentation/pages/results_page.dart';
+
+import 'more_info_dialog.dart';
+
+class ResultCard extends StatelessWidget {
+  final String title;
+
+  const ResultCard({
+    Key key,
+    @required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4,
+      child: Column(
+        children: [
+          buildTitleBar(context),
+          buildContent(context),
+        ],
+      ),
+    );
+  }
+
+  Container buildTitleBar(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.blueGrey.shade800,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 15),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                fontStyle: FontStyle.italic,
+                color: Colors.white,
+              ),
+            ),
+            IconButton(
+              icon: Icon(Icons.info_outline),
+              color: Colors.white,
+              iconSize: 16,
+              onPressed: () {
+                showDialog(
+                  barrierDismissible: true,
+                  context: context,
+                  builder: (_) => MoreInfoDialog(title: title),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  buildContent(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        FlatButton(
+          textColor: Colors.black87,
+          child: Text(
+            'View',
+            style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+          ),
+          onPressed: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (_) => ResultsPage()));
+          },
+        ),
+      ],
+    );
+  }
+}
