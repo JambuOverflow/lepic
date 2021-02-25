7bfb8e9bcaf1659bba52c3b00224a02067f0aaf4
João Canavarro <jvcanavarro@gmail.com>
2021-02-25 17:06:34 -0300

    Update statistic injection container

--- mobile/lib/injection_containers/statistic_injection_container.dart
+++ mobile/lib/injection_containers/statistic_injection_container.dart
@@ -1,6 +1,8 @@
 import 'package:get_it/get_it.dart';
 import 'package:mobile/features/audio_management/domain/entities/audio.dart';
+import 'package:mobile/features/audio_management/domain/use_cases/get_audio_from_id_use_case.dart';
 import 'package:mobile/features/statistics/domain/use_cases/get_number_of_correct_words_read_per_minute_use_case.dart';
+import 'package:mobile/features/statistics/domain/use_cases/get_number_of_words_read_per_minute_use_case.dart';
 import 'package:mobile/features/statistics/presentation/bloc/statistic_bloc.dart';
 import 'package:mobile/features/student_management/domain/entities/student.dart';
 import 'package:mobile/features/text_correction/domain/use_cases/get_correction_from_id_use_case.dart';
@@ -19,8 +21,14 @@ void init() {
   );
 
   getIt.registerLazySingleton(() => GetTextUseCase(repository: getIt()));
+  getIt.registerLazySingleton(() => GetAudioFromIdUseCase(repository: getIt()));
+
+  getIt.registerLazySingleton(
+      () => GetNumberOfWordsReadPerMinuteUseCase(getTextUseCase: getIt()));
+
   getIt.registerLazySingleton(
       () => GetCorrectionFromIdUseCase(repository: getIt()));
+
   getIt.registerLazySingleton(() => GetNumberOfCorrectWordsReadPerMinuteUseCase(
-      getCorrectionFromIdUseCase: getIt(), getTextUseCase: getIt()));
+      getAudioFromIdUseCase: getIt(), getTextUseCase: getIt()));
 }
