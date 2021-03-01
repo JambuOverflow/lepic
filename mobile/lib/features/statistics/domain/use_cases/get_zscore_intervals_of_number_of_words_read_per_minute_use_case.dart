9594b0500921f54941f4892a16dc71bbb2f15ad8
VitorCantao <vitornovaes.cantao@gmail.com>
2021-02-28 22:45:12 -0300

    Fix bug where wrong use case is used and add type notation to map

--- mobile/lib/features/statistics/domain/use_cases/get_zscore_intervals_of_number_of_words_read_per_minute_use_case.dart
+++ mobile/lib/features/statistics/domain/use_cases/get_zscore_intervals_of_number_of_words_read_per_minute_use_case.dart
@@ -4,13 +4,13 @@ import 'package:mobile/core/error/failures.dart';
 import 'package:mobile/core/use_cases/use_case.dart';
 import 'package:mobile/features/class_management/domain/entities/classroom.dart';
 import 'package:mobile/features/class_management/domain/use_cases/classroom_params.dart';
-import 'package:mobile/features/statistics/domain/use_cases/get_class_list_of_number_of_correct_words_read_per_minute_use_case.dart';
+import 'package:mobile/features/statistics/domain/use_cases/get_class_list_of_number_of_words_read_per_minute_use_case.dart';
 import 'package:mobile/features/statistics/domain/use_cases/get_zscore_of_number_of_words_read_per_minute_use_case.dart';
 import 'package:stats/stats.dart';
 
 class GetZscoreIntervalsOfNumberOfWordsReadPerMinuteUseCase
     implements UseCase<Map, ClassroomParams> {
-  final GetClassListOfCorrectNumberOfWordsReadPerMinuteUseCase
+  final GetClassListOfNumberOfWordsReadPerMinuteUseCase
       getClassListOfNumberOfWordsReadPerMinuteUseCase;
 
   GetZscoreIntervalsOfNumberOfWordsReadPerMinuteUseCase({
@@ -18,7 +18,8 @@ class GetZscoreIntervalsOfNumberOfWordsReadPerMinuteUseCase
   });
 
   @override
-  Future<Either<Failure, Map>> call(ClassroomParams params) async {
+  Future<Either<Failure, Map<ReadingStatus, double>>> call(
+      ClassroomParams params) async {
     try {
       List<double> numberOfWordsReadPerMinute =
           await _getClassroomStatistic(params.classroom);
@@ -31,7 +32,7 @@ class GetZscoreIntervalsOfNumberOfWordsReadPerMinuteUseCase
       final double mean = stats.average;
       final double standardDevation = stats.standardDeviation;
 
-      final Map result = {
+      final Map<ReadingStatus, double> result = {
         ReadingStatus.majorDeficit: _getValueFromZscore(
           standardDevation,
           mean,
@@ -70,9 +71,12 @@ class GetZscoreIntervalsOfNumberOfWordsReadPerMinuteUseCase
   Future<List<double>> _getClassroomStatistic(Classroom classroom) async {
     final ClassroomParams classroomParams =
         ClassroomParams(classroom: classroom);
+
     List<double> numberOfWordsReadPerMinute;
+
     final statisticOutput =
         await getClassListOfNumberOfWordsReadPerMinuteUseCase(classroomParams);
+
     statisticOutput.fold((l) {
       throw Exception();
     }, (r) {
