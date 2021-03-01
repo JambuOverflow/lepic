5bde3c05dbc49552507de6164457a2d99eebeb29
VitorCantao <vitornovaes.cantao@gmail.com>
2021-03-01 18:06:46 -0300

    Fix validation bug

--- mobile/lib/features/text_management/presentation/widgets/text_body_field.dart
+++ mobile/lib/features/text_management/presentation/widgets/text_body_field.dart
@@ -24,7 +24,8 @@ class TextBodyField extends StatelessWidget {
         floatingLabelBehavior: FloatingLabelBehavior.never,
       ),
       validator: (value) {
-        if (!value.contains('.*[a-zA-Z].*')) return 'Please enter a valid text';
+        if (value.contains(new RegExp('.*[a-zA-Z].*')) == false)
+          return 'Please enter a valid text';
         if (value.isEmpty) return 'A text body is required';
         return null;
       },
