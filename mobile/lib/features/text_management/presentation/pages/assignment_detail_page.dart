719cbdee977619effcd2435c917e60e049cafcd3
VitorCantao <vitornovaes.cantao@gmail.com>
2021-03-02 00:54:23 -0300

    Fix wrong merge

--- mobile/lib/features/text_management/presentation/pages/assignment_detail_page.dart
+++ mobile/lib/features/text_management/presentation/pages/assignment_detail_page.dart
@@ -1,20 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_it/get_it.dart';
 
 import '../../../statistics/presentation/bloc/statistic_bloc.dart';
 import '../widgets/assignment_popup_menu_button.dart';
 import '../widgets/statistics_preview_card.dart';
 import '../../../audio_management/presentation/bloc/audio_bloc.dart';
 import '../bloc/single_text_cubit.dart';
-import '../../../audio_management/presentation/bloc/player_cubit.dart';
 import '../bloc/assignment_status_cubit.dart';
-import '../../../text_correction/domain/use_cases/get_correction_use_case.dart';
 import '../../../text_correction/presentation/bloc/correction_bloc.dart';
-import 'package:mobile/features/audio_management/presentation/bloc/player_cubit.dart';
-import 'package:mobile/features/text_management/presentation/widgets/statistics_preview_card.dart';
-import 'package:mobile/features/audio_management/presentation/bloc/audio_bloc.dart';
-import '../../../audio_management/presentation/bloc/audio_bloc.dart';
 import '../widgets/assignment_contextual_floating_action_button.dart';
 import '../widgets/audio_preview_card.dart';
 import '../widgets/correction_preview_cards.dart';
@@ -52,6 +45,7 @@ class _AssigmentDetailPageState extends State<AssigmentDetailPage> {
   void initState() {
     textBloc = BlocProvider.of<TextBloc>(context);
     audioBloc = BlocProvider.of<AudioBloc>(context);
+    statusCubit = BlocProvider.of<AssignmentStatusCubit>(context);
 
     student = textBloc.student;
     super.initState();
@@ -64,62 +58,27 @@ class _AssigmentDetailPageState extends State<AssigmentDetailPage> {
     return BlocBuilder<TextBloc, TextState>(
       builder: (context, state) {
         audioBloc.add(LoadAudioEvent());
+
         return Scaffold(
           appBar: AppBar(
             title: Text('${student.firstName} ${student.lastName}'),
             bottom: AppBarBottomTextTitle(title: text.title),
+            actions: [AssignmentPopupMenuButton()],
           ),
-        ),
-        BlocProvider(
-          lazy: false,
-          create: (_) => audioBloc = GetIt.instance.get<AudioBloc>(
-            param1: StudentTextParams(text: text, student: student),
-          ),
-        ),
-        BlocProvider(
-          create: (_) => PlayerCubit(audioBloc),
-        ),
-        BlocProvider(
-          lazy: false,
-          create: (_) => statisticBloc = GetIt.instance.get<StatisticBloc>(
-            param1: student,
-            param2: audioBloc.audio,
-          ),
-        ),
-        BlocProvider(
-          lazy: false,
-          create: (_) => statusCubit = AssignmentStatusCubit(
-            audioBloc: audioBloc,
-            textBloc: textBloc,
-            correctionBloc: correctionBloc,
-          ),
-        ),
-      ],
-      child: BlocBuilder<TextBloc, TextState>(
-        builder: (context, state) {
-          audioBloc.add(LoadAudioEvent());
-
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('${student.firstName} ${student.lastName}'),
-              bottom: AppBarBottomTextTitle(title: text.title),
-              actions: [AssignmentPopupMenuButton()],
-            ),
-            floatingActionButtonLocation:
-                FloatingActionButtonLocation.centerFloat,
-            floatingActionButton: AssignmentContextualFloatingActionButton(),
-            body: Scrollbar(
-              controller: _scrollController,
-              isAlwaysShown: true,
-              child: BlocBuilder<AssignmentStatusCubit, AssignmentStatus>(
-                builder: (context, state) {
-                  return ListView(
-                    controller: _scrollController,
-                    padding: EdgeInsets.all(16),
-                    children: _getViewBasedOnAvailability(),
-                  );
-                },
-              ),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerFloat,
+          floatingActionButton: AssignmentContextualFloatingActionButton(),
+          body: Scrollbar(
+            controller: _scrollController,
+            isAlwaysShown: true,
+            child: BlocBuilder<AssignmentStatusCubit, AssignmentStatus>(
+              builder: (context, state) {
+                return ListView(
+                  controller: _scrollController,
+                  padding: EdgeInsets.all(16),
+                  children: _getViewBasedOnAvailability(),
+                );
+              },
             ),
           ),
         );
